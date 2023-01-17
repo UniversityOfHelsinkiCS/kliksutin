c53b2416a638c82087edf7314399b0f80f411478
Henri Remonen <henri@remonen.fi>
2023-01-17 15:43:50 +0200

    Move Development data to data folder from component

--- src/client/components/Form/Form.tsx
+++ src/client/components/Form/Form.tsx
@@ -3,63 +3,15 @@ import { Button, Container } from '@mui/material'
 import { useForm } from 'react-hook-form'
 import FormOptionBox from './FormOptionBox'
 
-const questions = [
-  {
-    id: 1,
-    heading: 'First question',
-    text: 'This is the first question',
-    options: [
-      {
-        label: 'Radio Option 1',
-        value: 'Option 1',
-      },
-      {
-        label: 'Radio Option 2',
-        value: 'Option 2',
-      },
-    ],
-  },
-  {
-    id: 2,
-    heading: 'Second question',
-    text: 'This is the second question',
-    options: [
-      {
-        label: 'Radio Option 1',
-        value: 'Option 1',
-      },
-      {
-        label: 'Radio Option 2',
-        value: 'Option 2',
-      },
-      {
-        label: 'Radio Option 3',
-        value: 'Option 3',
-      },
-    ],
-  },
-  {
-    id: 3,
-    heading: 'Third and the last question',
-    text: 'This is the third question',
-    options: [
-      {
-        label: 'Radio Option 1',
-        value: 'Option 1',
-      },
-      {
-        label: 'Radio Option 2',
-        value: 'Option 2',
-      },
-    ],
-  },
-]
+import getQuestionData from '../../data/devQuestionData'
 
 const Form = () => {
   const { handleSubmit, control } = useForm({
     mode: 'onBlur',
   })
   const onSubmit = (data: any) => console.log(data)
+
+  const questions = getQuestionData()
 
   return (
     <Container>
---
+++ src/client/data/devQuestionData.tsx
@@ -0,0 +1,53 @@
+const getQuestionData = () => [
+  {
+    id: 1,
+    heading: 'First question',
+    text: 'This is the first question',
+    options: [
+      {
+        label: 'Radio Option 1',
+        value: 'Option 1',
+      },
+      {
+        label: 'Radio Option 2',
+        value: 'Option 2',
+      },
+    ],
+  },
+  {
+    id: 2,
+    heading: 'Second question',
+    text: 'This is the second question',
+    options: [
+      {
+        label: 'Radio Option 1',
+        value: 'Option 1',
+      },
+      {
+        label: 'Radio Option 2',
+        value: 'Option 2',
+      },
+      {
+        label: 'Radio Option 3',
+        value: 'Option 3',
+      },
+    ],
+  },
+  {
+    id: 3,
+    heading: 'Third and the last question',
+    text: 'This is the third question',
+    options: [
+      {
+        label: 'Radio Option 1',
+        value: 'Option 1',
+      },
+      {
+        label: 'Radio Option 2',
+        value: 'Option 2',
+      },
+    ],
+  },
+]
+
+export default getQuestionData
