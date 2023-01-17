dc3f7f64acca72b0bf8be298416867107d187fb5
Henri Remonen <henri@remonen.fi>
2023-01-17 12:37:08 +0200

    Remove kliksauttelu from the app

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,43 +1,13 @@
-import React from 'react'
-import logo from './assets/toscalogo_color.svg'
 import './App.css'
 import axios from 'axios'
 import Form from './components/Form'
 
 const basePath = process.env.PUBLIC_URL || ''
-const apiClient = axios.create({ baseURL: `${basePath}/api` })
 
 const App = () => {
-  const [response, setResponse] = React.useState({
-    message: '...',
-    timeoutId: 0,
-  })
-
-  const handleClick = async () => {
-    if (response.timeoutId) {
-      clearTimeout(response.timeoutId)
-    }
-    const { data } = await apiClient.get('/kliks')
-    const timeoutId = setTimeout(
-      () => setResponse({ message: '...', timeoutId: 0 }),
-      3000
-    ) as unknown as number
-    setResponse({ message: data, timeoutId: timeoutId })
-  }
-
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Kliksutin</h1>
-        <button
-          onClick={handleClick}
-          className="App-button"
-          data-cy="theButton"
-        >
-          Click to klik
-        </button>
-        <p>kliksutin says: {response.message}</p>
         <Form />
       </header>
     </div>
