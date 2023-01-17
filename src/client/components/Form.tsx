82ca89d3e00f11af86b49d8e0d61899ef6a83944
Henri Remonen <henri@remonen.fi>
2023-01-17 14:07:28 +0200

    Change Form to use static JSON data

--- src/client/components/Form.tsx
+++ src/client/components/Form.tsx
@@ -3,14 +3,55 @@ import { Button, Container } from '@mui/material'
 import { useForm } from 'react-hook-form'
 import FormOptionBox from './FormOptionBox'
 
-const options = [
+const questions = [
   {
-    label: 'Radio Option 1',
-    value: 'Option 1',
+    id: 1,
+    heading: 'First question',
+    text: 'This is the first question',
+    options: [
+      {
+        label: 'Radio Option 1',
+        value: 'Option 1',
+      },
+      {
+        label: 'Radio Option 2',
+        value: 'Option 2',
+      },
+    ],
   },
   {
-    label: 'Radio Option 2',
-    value: 'Option 2',
+    id: 2,
+    heading: 'Second question',
+    text: 'This is the second question',
+    options: [
+      {
+        label: 'Radio Option 1',
+        value: 'Option 1',
+      },
+      {
+        label: 'Radio Option 2',
+        value: 'Option 2',
+      },
+      {
+        label: 'Radio Option 3',
+        value: 'Option 3',
+      },
+    ],
+  },
+  {
+    id: 3,
+    heading: 'Third and the last question',
+    text: 'This is the third question',
+    options: [
+      {
+        label: 'Radio Option 1',
+        value: 'Option 1',
+      },
+      {
+        label: 'Radio Option 2',
+        value: 'Option 2',
+      },
+    ],
   },
 ]
 
@@ -23,7 +64,13 @@ const Form = () => {
   return (
     <Container>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <FormOptionBox control={control} options={options} />
+        {questions.map((singleQuestion) => (
+          <FormOptionBox
+            key={singleQuestion.id}
+            control={control}
+            questionData={singleQuestion}
+          />
+        ))}
         <Button onClick={handleSubmit(onSubmit)}>Submit</Button>
       </form>
     </Container>
