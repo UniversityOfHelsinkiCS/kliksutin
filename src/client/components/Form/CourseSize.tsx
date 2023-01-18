7cbc483c6600d45f22b2afe6cf555c270939e379
Henri Remonen <henri@remonen.fi>
2023-01-18 15:08:11 +0200

    Pivot Form creation to use hard coded components for now

---
+++ src/client/components/Form/CourseSize.tsx
@@ -0,0 +1,113 @@
+import React from 'react'
+import {
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  CardContent,
+  Typography,
+  Card,
+  Box,
+} from '@mui/material'
+import { Controller } from 'react-hook-form'
+import { InputProps } from '../../types'
+
+const CourseSize: React.FC<InputProps> = ({ control, watch }) => {
+  const largeSize = watch('courseAttendants')
+  const mooc = watch('isMooc')
+
+  console.log(largeSize)
+
+  return (
+    <Box sx={{ maxWidth: 1080 }}>
+      <Card variant="outlined">
+        <CardContent>
+          <Typography variant="h5" component="div">
+            Kurssin koko
+          </Typography>
+          <Box>
+            <Typography variant="body2">
+              Isolle kurssille sopii hyvin alustaksi Moodle. Voit lisätä
+              vuorovaikutusmahdollisuuksia luonnoilla Presemon avulla.
+            </Typography>
+            <Typography variant="body2">
+              Mikäli haluat toteuttaa kurssisi MOOC-muodossa, katso tästä
+              tarkemmat ohjeet.
+            </Typography>
+          </Box>
+        </CardContent>
+      </Card>
+
+      <Controller
+        control={control}
+        name="courseAttendants"
+        defaultValue=""
+        render={({ field }) => (
+          <Box display="flex" justifyContent="center">
+            <RadioGroup {...field} row>
+              <FormControlLabel
+                value="smallSize"
+                label="< 50"
+                name="smallSize"
+                control={<Radio />}
+              />
+              <FormControlLabel
+                value="largeSize"
+                label="> 50"
+                control={<Radio />}
+              />
+            </RadioGroup>
+          </Box>
+        )}
+      />
+
+      {largeSize !== 'largeSize' && largeSize && (
+        <Typography variant="body2">
+          Suosittelemme valitsemaan Moodlen alustaksi.
+        </Typography>
+      )}
+
+      {largeSize === 'largeSize' && (
+        <Box>
+          <Typography variant="body2">
+            Haluatko pitää kurssin MOOCina?
+          </Typography>
+
+          <Controller
+            control={control}
+            name="isMooc"
+            defaultValue=""
+            render={({ field }) => (
+              <Box display="flex" justifyContent="center">
+                <RadioGroup {...field} row>
+                  <FormControlLabel
+                    value="true"
+                    label="Kyllä"
+                    name="mooc"
+                    control={<Radio />}
+                  />
+                  <FormControlLabel
+                    value="false"
+                    label="Ei"
+                    control={<Radio />}
+                  />
+                </RadioGroup>
+              </Box>
+            )}
+          />
+          {mooc === 'true' ? (
+            <Typography variant="body2">
+              Suosittelemme valitsemaan DigiCapuksen tai KumpulaMoocin
+              alustaksi.
+            </Typography>
+          ) : (
+            <Typography variant="body2">
+              Suosittelemme valitsemaan Moodlen alustaksi.
+            </Typography>
+          )}
+        </Box>
+      )}
+    </Box>
+  )
+}
+
+export default CourseSize
--- src/client/components/Form/Form.tsx
+++ src/client/components/Form/Form.tsx
@@ -1,28 +1,19 @@
 import React from 'react'
 import { Button, Container } from '@mui/material'
 import { useForm } from 'react-hook-form'
-import FormOptionBox from './FormOptionBox'
 
-import getQuestionData from '../../data/devQuestionData'
+import CourseSize from './CourseSize'
 
 const Form = () => {
-  const { handleSubmit, control } = useForm({
+  const { handleSubmit, control, watch } = useForm({
     mode: 'onBlur',
   })
   const onSubmit = (data: any) => console.log(data)
 
-  const questions = getQuestionData()
-
   return (
     <Container>
       <form onSubmit={handleSubmit(onSubmit)}>
-        {questions.map((singleQuestion) => (
-          <FormOptionBox
-            key={singleQuestion.id}
-            control={control}
-            questionData={singleQuestion}
-          />
-        ))}
+        <CourseSize control={control} watch={watch} />
         <Button onClick={handleSubmit(onSubmit)}>Submit</Button>
       </form>
     </Container>
