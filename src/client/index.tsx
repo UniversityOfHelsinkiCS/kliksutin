4b853d14935fd4e387fb2041cff1fe4eabd4e702
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-01-19 10:52:54 +0200

    Initialize react-i18next for localization

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import CssBaseline from '@mui/material/CssBaseline'
+
 import App from './App'
+import initializeI18n from './util/il18n'
+
+initializeI18n()
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
