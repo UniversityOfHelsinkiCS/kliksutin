75d95e097114dc08e355a09b9845d70f8d80f46e
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-01-19 13:03:26 +0200

    Only use Sentry in production

--- src/client/util/sentry.ts
+++ src/client/util/sentry.ts
@@ -1,9 +1,9 @@
 import * as Sentry from '@sentry/browser'
 import { Integrations } from '@sentry/tracing'
-import { /* inProduction, inStaging, inE2EMode, */ GIT_SHA } from '../../config'
+import { inProduction, inStaging, inE2EMode, GIT_SHA } from '../../config'
 
 const initializeSentry = () => {
-  // if (!inProduction || inStaging || inE2EMode) return
+  if (!inProduction || inStaging || inE2EMode) return
 
   Sentry.init({
     dsn: 'https://df9acc7c370a4a7396e99888a8f3eadb@sentry.cs.helsinki.fi/15',
