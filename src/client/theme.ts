9578214e8df6a4bbc6402fd7cb98c820486c2955
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-01-19 10:02:37 +0200

    Use MUI ThemeProvider, remove default css

--- src/client/theme.ts
+++ src/client/theme.ts
@@ -2,3 +2,14 @@ import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
+
+import { useMemo } from 'react'
+import { createTheme, responsiveFontSizes } from '@mui/material/styles'
+
+const useTheme = () => {
+  const theme = useMemo(() => responsiveFontSizes(createTheme({})), [])
+
+  return theme
+}
+
+export default useTheme
