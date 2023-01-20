a7f707a9ff582ed083d7fd5f53c92f9dceb26dd6
Henri Remonen <henri@remonen.fi>
2023-01-20 12:59:18 +0200

    Move NavBar styles to independent file

--- src/client/components/NavBar/NavBar.tsx
+++ src/client/components/NavBar/NavBar.tsx
@@ -13,64 +13,10 @@ import {
   Popper,
   Divider,
 } from '@mui/material'
-import { Theme } from '@mui/material/styles'
 import { useTranslation } from 'react-i18next'
 
 import toscalogoColor from '../../assets/toscalogo_color.svg'
-
-const styles = {
-  toolbar: {
-    display: 'flex',
-    width: '100%',
-    '@media print': {
-      display: 'none',
-    },
-    padding: '0.2rem 0 0.2rem 0',
-  },
-  link: {
-    display: 'inline-flex',
-    alignItems: 'center',
-    color: 'inherit',
-    textDecoration: 'none',
-    marginRight: 1,
-    fontWeight: (theme: Theme) => theme.typography.fontWeightMedium,
-    padding: '5px 12px',
-    backgroundColor: 'rgba(255, 255, 255, 0)',
-    transition: 'background-color 0.1s',
-    borderRadius: 3,
-    '&:hover': {
-      backgroundColor: 'rgba(255, 255, 255, 0.22)',
-    },
-  },
-  activeLink: {
-    backgroundColor: 'rgba(255, 255, 255, 0.15)',
-  },
-  linkContainer: {
-    display: 'flex',
-    flexGrow: 1,
-    alignItems: 'center',
-  },
-  menuButton: {
-    marginLeft: 'auto',
-    '&:hover': {
-      backgroundColor: 'rgba(255, 255, 255, 0.22)',
-    },
-  },
-  languageMenuDivider: {
-    margin: (theme: Theme) => theme.spacing(1, 0),
-  },
-  container: {
-    display: 'flex',
-  },
-  item: {
-    flexGrow: 1,
-    justifyContent: 'center',
-  },
-  activeItem: {
-    color: (theme: Theme) => theme.palette.primary.main,
-    fontWeight: (theme: Theme) => theme.typography.fontWeightMedium,
-  },
-}
+import styles from './styles'
 
 const NavBar = () => {
   const { t, i18n } = useTranslation()
@@ -89,6 +35,8 @@ const NavBar = () => {
     setOpenLanguageSelect(false)
   }
 
+  const classes = styles.navStyles
+
   return (
     <AppBar
       elevation={0}
@@ -101,8 +49,8 @@ const NavBar = () => {
       }}
     >
       <Container maxWidth={false}>
-        <Toolbar sx={styles.toolbar} disableGutters>
-          <Box display="inline-flex" alignItems="end" sx={styles.link}>
+        <Toolbar sx={classes.toolbar} disableGutters>
+          <Box display="inline-flex" alignItems="end" sx={classes.link}>
             <img src={toscalogoColor} alt="Toska" width="80" />
             <Box
               ml="1rem"
@@ -159,8 +107,8 @@ const NavBar = () => {
                           <MenuItem
                             key={l}
                             sx={[
-                              styles.item,
-                              language === l && styles.activeItem,
+                              classes.item,
+                              language === l && classes.activeItem,
                             ]}
                             onClick={() => {
                               handleLanguageChange(l)
