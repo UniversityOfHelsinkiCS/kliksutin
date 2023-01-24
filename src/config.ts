c04f615e89b3a8a0b6584c4681a4726d742ddf99
Henri Remonen <henri@remonen.fi>
2023-01-24 12:36:59 +0200

    Add JAMI_URL to the config

--- src/config.ts
+++ src/config.ts
@@ -5,3 +5,7 @@ export const inStaging = process.env.REACT_APP_STAGING === 'true'
 export const inE2EMode = process.env.REACT_APP_E2E === 'true'
 
 export const GIT_SHA = process.env.REACT_APP_GIT_SHA || ''
+
+export const JAMI_URL = inProduction
+  ? 'https://importer.cs.helsinki.fi/api/auth'
+  : 'https://toska-staging.cs.helsinki.fi/jami'
