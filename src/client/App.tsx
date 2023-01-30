608a8a26bf1097c534b86589e197de701bf6a7a3
Henri Remonen <henri@remonen.fi>
2023-01-30 14:31:02 +0200

    Render old form in production

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { ThemeProvider } from '@mui/material/styles'
 import { Box } from '@mui/material'
-
+import { inProduction, inStaging } from '../config'
 import useTheme from './theme'
 import Footer from './components/Footer'
 import NavBar from './components/NavBar/NavBar'
 import InteractiveForm from './components/InteractiveForm/InteractiveForm'
+import Form from './components/Form/Form'
 
 const App = () => {
   const theme = useTheme()
@@ -14,7 +15,7 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <Box textAlign="center">
         <NavBar />
-        <InteractiveForm />
+        {inProduction || inStaging ? <Form /> : <InteractiveForm />}
         <Footer />
       </Box>
     </ThemeProvider>
