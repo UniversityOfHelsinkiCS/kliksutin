dda199f5bb1ffee6a27c25d06fd47d64e9b2e6c0
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-02-01 13:00:39 +0200

    Show interactive form in production

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import { ThemeProvider } from '@mui/material/styles'
 import { Box } from '@mui/material'
-import { inProduction } from '../config'
 import useTheme from './theme'
 import Footer from './components/Footer'
 import NavBar from './components/NavBar/NavBar'
 import InteractiveForm from './components/InteractiveForm/InteractiveForm'
-import Form from './components/Form/Form'
 
 const App = () => {
   const theme = useTheme()
@@ -15,7 +13,7 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <Box textAlign="center">
         <NavBar />
-        {inProduction ? <Form /> : <InteractiveForm />}
+        <InteractiveForm />
         <Footer />
       </Box>
     </ThemeProvider>
