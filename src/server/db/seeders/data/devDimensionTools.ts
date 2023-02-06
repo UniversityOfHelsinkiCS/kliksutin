8cc04ae711bdeb40e83b9d2300535e7c6447987d
Henri Remonen <henri@remonen.fi>
2023-02-06 15:58:36 +0200

    Create data for dimensions

---
+++ src/server/db/seeders/data/devDimensionTools.ts
@@ -0,0 +1,121 @@
+const getDimensionData = () => [
+  {
+    id: 1,
+    title: {
+      fi: 'Zoom',
+      sv: 'Zoom',
+      en: 'Zoom',
+    },
+    text: {
+      fi: 'Esittelyteksti liittyen tähän työkaluun',
+      sv: 'Esittelyteksti liittyen tähän työkaluun',
+      en: 'Esittelyteksti liittyen tähän työkaluun',
+    },
+  },
+  {
+    id: 2,
+    title: {
+      fi: 'Unitube',
+      sv: 'Unitube',
+      en: 'Unitube',
+    },
+    text: {
+      fi: 'Esittelyteksti liittyen tähän työkaluun',
+      sv: 'Esittelyteksti liittyen tähän työkaluun',
+      en: 'Esittelyteksti liittyen tähän työkaluun',
+    },
+  },
+  {
+    id: 3,
+    title: {
+      fi: 'Flinga',
+      sv: 'Flinga',
+      en: 'Flinga',
+    },
+    text: {
+      fi: 'Esittelyteksti liittyen tähän työkaluun',
+      sv: 'Esittelyteksti liittyen tähän työkaluun',
+      en: 'Esittelyteksti liittyen tähän työkaluun',
+    },
+  },
+  {
+    id: 4,
+    title: {
+      fi: 'Presemo',
+      sv: 'Presemo',
+      en: 'Presemo',
+    },
+    text: {
+      fi: 'Esittelyteksti liittyen tähän työkaluun',
+      sv: 'Esittelyteksti liittyen tähän työkaluun',
+      en: 'Esittelyteksti liittyen tähän työkaluun',
+    },
+  },
+  {
+    id: 5,
+    title: {
+      fi: 'Thinklink',
+      sv: 'Thinklink',
+      en: 'Thinklink',
+    },
+    text: {
+      fi: 'Esittelyteksti liittyen tähän työkaluun',
+      sv: 'Esittelyteksti liittyen tähän työkaluun',
+      en: 'Esittelyteksti liittyen tähän työkaluun',
+    },
+  },
+  {
+    id: 6,
+    title: {
+      fi: 'Eportfolio',
+      sv: 'Eportfolio',
+      en: 'Eportfolio',
+    },
+    text: {
+      fi: 'Esittelyteksti liittyen tähän työkaluun',
+      sv: 'Esittelyteksti liittyen tähän työkaluun',
+      en: 'Esittelyteksti liittyen tähän työkaluun',
+    },
+  },
+  {
+    id: 7,
+    title: {
+      fi: 'Moodle',
+      sv: 'Moodle',
+      en: 'Moodle',
+    },
+    text: {
+      fi: 'Esittelyteksti liittyen tähän työkaluun',
+      sv: 'Esittelyteksti liittyen tähän työkaluun',
+      en: 'Esittelyteksti liittyen tähän työkaluun',
+    },
+  },
+  {
+    id: 8,
+    title: {
+      fi: 'Screen-cast-o-matic',
+      sv: 'Screen-cast-o-matic',
+      en: 'Screen-cast-o-matic',
+    },
+    text: {
+      fi: 'Esittelyteksti liittyen tähän työkaluun',
+      sv: 'Esittelyteksti liittyen tähän työkaluun',
+      en: 'Esittelyteksti liittyen tähän työkaluun',
+    },
+  },
+  {
+    id: 9,
+    title: {
+      fi: 'H5P',
+      sv: 'H5P',
+      en: 'H5P',
+    },
+    text: {
+      fi: 'Esittelyteksti liittyen tähän työkaluun',
+      sv: 'Esittelyteksti liittyen tähän työkaluun',
+      en: 'Esittelyteksti liittyen tähän työkaluun',
+    },
+  },
+]
+
+export default getDimensionData
