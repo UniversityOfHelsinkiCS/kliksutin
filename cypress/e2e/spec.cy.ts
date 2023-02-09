f4f5914ba5dcc01588beeb2694f02bcd0987c8ac
Henri Remonen <henri@remonen.fi>
2023-02-09 09:47:03 +0200

    Better testing for now

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,20 +1,44 @@
 import { baseUrl } from '../support/e2e'
 
 import getQuestionData from '../../src/server/db/seeders/data/devQuestionData'
+import { Question } from '../../src/client/types'
 
 describe('Kliksutin web page', () => {
-  beforeEach(() => cy.visit(baseUrl))
+  let questionData
 
-  it.skip('loads the main page', () => {
-    cy.contains('Hi, welcome to Kliksutin')
+  it('loads the main page', () => {
+    cy.visit(baseUrl)
+
+    cy.contains('Kliksutin')
   })
 
-  it('form loads correctly', () => {
+  it('loads visible questions correctly', () => {
     cy.visit(baseUrl)
-    cy.contains('Kurssin koko')
-    cy.contains('Osallistuminen')
-    cy.contains('Tallennus')
-    cy.contains('Suoritusmuoto')
-    cy.contains('Arviointi')
+
+    questionData = getQuestionData()
+
+    cy.wrap(questionData).each((question: Question) => {
+      if (!question.visibility.options) {
+        cy.contains(question.title.en)
+      }
+    })
+  })
+
+  it('does not load invisible questions before they have been touched', () => {
+    cy.visit(baseUrl)
+
+    questionData = getQuestionData()
+
+    cy.wrap(questionData).each((question: Question) => {
+      if (question.visibility.options) {
+        cy.contains(question.title.en).should('not.exist')
+      }
+    })
+  })
+
+  it('has spot for recommendations', () => {
+    cy.visit(baseUrl)
+
+    cy.contains('Dimensions that support your selections')
   })
 })
