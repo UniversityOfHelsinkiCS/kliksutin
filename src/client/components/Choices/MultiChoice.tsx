d12e9b3212d41409e38b0b42cd380821b88d911d
Henri Remonen <henri@remonen.fi>
2023-02-10 10:12:31 +0200

    Change multiplechoice component to checkbox style

--- src/client/components/Choices/MultiChoice.tsx
+++ src/client/components/Choices/MultiChoice.tsx
@@ -1,26 +1,7 @@
 import React from 'react'
 import { Controller } from 'react-hook-form'
-import {
-  Box,
-  OutlinedInput,
-  InputLabel,
-  MenuItem,
-  FormControl,
-  Select,
-} from '@mui/material'
-import Chip from '@mui/material/Chip'
+import { Box, FormControl, Checkbox, FormControlLabel } from '@mui/material'
 import { MultipleChoiceType, Question } from '../../types'
-
-const ITEM_HEIGHT = 48
-const ITEM_PADDING_TOP = 8
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-}
 
 const MultiChoice: React.FC<{
   control: any
@@ -30,45 +11,30 @@ const MultiChoice: React.FC<{
   language: string
 }> = ({ control, question, children, language }) => (
   <>
-    <Controller
-      name={question.id.toString()}
-      control={control}
-      defaultValue={[]}
-      render={({ field }) => (
-        <FormControl sx={{ width: '100%' }}>
-          <InputLabel id={`multiple-choise-label-${question.id}`}>
-            {question.title[language]}
-          </InputLabel>
-          <Select
-            labelId={`multiple-choise-label-${question.id}`}
-            multiple
-            value={field.value}
-            input={<OutlinedInput label={question.title[language]} />}
-            {...field}
-            renderValue={(selected) => (
-              <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-                {selected.map((selectedChoise) => {
-                  const parsedChoise = JSON.parse(selectedChoise)
-                  return (
-                    <Chip
-                      key={parsedChoise.id}
-                      label={parsedChoise.label[language]}
-                    />
-                  )
-                })}
-              </Box>
-            )}
-            MenuProps={MenuProps}
-          >
-            {question.optionData.options.map((choise: MultipleChoiceType) => (
-              <MenuItem key={choise.id} value={JSON.stringify(choise)}>
-                {choise.label[language]}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      )}
-    />
+    {question.optionData.options.map((choice: MultipleChoiceType) => (
+      <Controller
+        key={choice.id}
+        name={`${question.id}.${choice.id}`}
+        control={control}
+        defaultValue={false}
+        render={({ field }) => (
+          <FormControl sx={{ width: '100%' }}>
+            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.3 }}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    {...field}
+                    value={choice.id}
+                    checked={field.value}
+                  />
+                }
+                label={choice.label[language]}
+              />
+            </Box>
+          </FormControl>
+        )}
+      />
+    ))}
 
     {children}
   </>
