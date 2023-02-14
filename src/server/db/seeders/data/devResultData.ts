43ff92e059f7fff908db9fd0ddf0af70f90a3a7f
Henri Remonen <henri@remonen.fi>
2023-02-14 09:22:50 +0200

    Modify result data

--- src/server/db/seeders/data/devResultData.ts
+++ src/server/db/seeders/data/devResultData.ts
@@ -1,7 +1,12 @@
 const getResultData = () => [
   {
     courseSizeSmall: {
-      all: {
+      isSelected: {
+        fi: 'Olet valinnut kurssillesi osallistujamääräksi < 50 opiskelijaa.',
+        sv: 'Olet valinnut kurssillesi osallistujamääräksi < 50 opiskelijaa.',
+        en: 'Olet valinnut kurssillesi osallistujamääräksi < 50 opiskelijaa.',
+      },
+      allDimensions: {
         fi: 'Placeholder text',
         sv: 'Placeholder text',
         en: 'Placeholder text',
@@ -38,7 +43,12 @@ const getResultData = () => [
       },
     },
     courseSizeBig: {
-      all: {
+      isSelected: {
+        fi: 'Olet valinnut kurssillesi osallistujamääräksi >= 50 opiskelijaa.',
+        sv: 'Olet valinnut kurssillesi osallistujamääräksi >= 50 opiskelijaa.',
+        en: 'Olet valinnut kurssillesi osallistujamääräksi >= 50 opiskelijaa.',
+      },
+      allDimensions: {
         fi: 'Placeholder text',
         sv: 'Placeholder text',
         en: 'Placeholder text',
@@ -75,7 +85,12 @@ const getResultData = () => [
       },
     },
     courseSizeUnlimited: {
-      all: {
+      isSelected: {
+        fi: 'Kurssillesi ei ole määritelty maksimi opiskelijamäärää.',
+        sv: 'Kurssillesi ei ole määritelty maksimi opiskelijamäärää.',
+        en: 'Kurssillesi ei ole määritelty maksimi opiskelijamäärää.',
+      },
+      allDimensions: {
         fi: 'Placeholder text',
         sv: 'Placeholder text',
         en: 'Placeholder text',
@@ -111,8 +126,97 @@ const getResultData = () => [
         en: 'Placeholder text',
       },
     },
+    courseIsMooc: {
+      isSelected: {
+        fi: 'Olet valinnut kurssin MOOC-kurssiksi',
+        sv: 'Olet valinnut kurssin MOOC-kurssiksi',
+        en: 'Olet valinnut kurssin MOOC-kurssiksi',
+      },
+      allDimensions: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      investication: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      production: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      collaboration: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      discussion: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      acquisition: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      practice: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+    },
+    courseIsNotMooc: {
+      isSelected: {
+        fi: 'Olet valinnut ettei kurssi ole MOOC-kurssi.',
+        sv: 'Olet valinnut ettei kurssi ole MOOC-kurssi.',
+        en: 'Olet valinnut ettei kurssi ole MOOC-kurssi.',
+      },
+      allDimensions: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      investication: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      production: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      collaboration: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      discussion: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      acquisition: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      practice: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+    },
     courseAttendancePresent: {
-      all: {
+      isSelected: {
+        fi: 'Olet valinnut kurssillesi opetusmuodoksi lähiopetuksen.',
+        sv: 'Olet valinnut kurssillesi opetusmuodoksi lähiopetuksen.',
+        en: 'Olet valinnut kurssillesi opetusmuodoksi lähiopetuksen.',
+      },
+      allDimensions: {
         fi: 'Placeholder text',
         sv: 'Placeholder text',
         en: 'Placeholder text',
@@ -149,7 +253,12 @@ const getResultData = () => [
       },
     },
     courseAttendanceHybrid: {
-      all: {
+      isSelected: {
+        fi: 'Olet valinnut kurssillesi opetusmuodoksi hybridiopetuksen.',
+        sv: 'Olet valinnut kurssillesi opetusmuodoksi hybridiopetuksen.',
+        en: 'Olet valinnut kurssillesi opetusmuodoksi hybridiopetuksen.',
+      },
+      allDimensions: {
         fi: 'Placeholder text',
         sv: 'Placeholder text',
         en: 'Placeholder text',
@@ -186,7 +295,12 @@ const getResultData = () => [
       },
     },
     courseAttendanceRemote: {
-      all: {
+      isSelected: {
+        fi: 'Olet valinnut kurssillesi opetusmuodoksi etäopetuksen.',
+        sv: 'Olet valinnut kurssillesi opetusmuodoksi etäopetuksen.',
+        en: 'Olet valinnut kurssillesi opetusmuodoksi etäopetuksen.',
+      },
+      allDimensions: {
         fi: 'Placeholder text',
         sv: 'Placeholder text',
         en: 'Placeholder text',
@@ -222,8 +336,97 @@ const getResultData = () => [
         en: 'Placeholder text',
       },
     },
+    courseIsRecorded: {
+      isSelected: {
+        fi: 'Olet valinnut nauhoittaa kurssin luennot.',
+        sv: 'Olet valinnut nauhoittaa kurssin luennot.',
+        en: 'Olet valinnut nauhoittaa kurssin luennot.',
+      },
+      allDimensions: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      investication: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      production: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      collaboration: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      discussion: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      acquisition: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      practice: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+    },
+    courseIsNotRecorded: {
+      isSelected: {
+        fi: 'Olet valinnut ettei luentoja nauhoiteta.',
+        sv: 'Olet valinnut ettei luentoja nauhoiteta.',
+        en: 'Olet valinnut ettei luentoja nauhoiteta.',
+      },
+      allDimensions: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      investication: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      production: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      collaboration: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      discussion: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      acquisition: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+      practice: {
+        fi: '',
+        sv: '',
+        en: '',
+      },
+    },
     courseCompletionMethodExam: {
-      all: {
+      isSelected: {
+        fi: 'Olet valinnut kurssillesi suoritusmuodoksi tentin.',
+        sv: 'Olet valinnut kurssillesi suoritusmuodoksi tentin.',
+        en: 'Olet valinnut kurssillesi suoritusmuodoksi tentin.',
+      },
+      allDimensions: {
         fi: 'Placeholder text',
         sv: 'Placeholder text',
         en: 'Placeholder text',
@@ -260,7 +463,12 @@ const getResultData = () => [
       },
     },
     courseCompletionMethodDemo: {
-      all: {
+      isSelected: {
+        fi: 'Olet valinnut kurssillesi suoritusmuodoksi demot.',
+        sv: 'Olet valinnut kurssillesi suoritusmuodoksi demot.',
+        en: 'Olet valinnut kurssillesi suoritusmuodoksi demot.',
+      },
+      allDimensions: {
         fi: 'Placeholder text',
         sv: 'Placeholder text',
         en: 'Placeholder text',
@@ -297,7 +505,12 @@ const getResultData = () => [
       },
     },
     courseCompletionMethodDiary: {
-      all: {
+      isSelected: {
+        fi: 'Olet valinnut kurssillesi suoritusmuodoksi oppimispäiväkirjan.',
+        sv: 'Olet valinnut kurssillesi suoritusmuodoksi oppimispäiväkirjan.',
+        en: 'Olet valinnut kurssillesi suoritusmuodoksi oppimispäiväkirjan.',
+      },
+      allDimensions: {
         fi: 'Placeholder text',
         sv: 'Placeholder text',
         en: 'Placeholder text',
@@ -334,7 +547,12 @@ const getResultData = () => [
       },
     },
     courseCompletionMethodAssignment: {
-      all: {
+      isSelected: {
+        fi: 'Olet valinnut kurssillesi suoritusmuodoksi lopputyön.',
+        sv: 'Olet valinnut kurssillesi suoritusmuodoksi lopputyön.',
+        en: 'Olet valinnut kurssillesi suoritusmuodoksi lopputyön.',
+      },
+      allDimensions: {
         fi: 'Placeholder text',
         sv: 'Placeholder text',
         en: 'Placeholder text',
@@ -371,7 +589,12 @@ const getResultData = () => [
       },
     },
     courseCompletionMethodParticipation: {
-      all: {
+      isSelected: {
+        fi: 'Olet valinnut kurssillesi suoritusmuodoksi osallistumisen opetukseen.',
+        sv: 'Olet valinnut kurssillesi suoritusmuodoksi osallistumisen opetukseen.',
+        en: 'Olet valinnut kurssillesi suoritusmuodoksi osallistumisen opetukseen.',
+      },
+      allDimensions: {
         fi: 'Placeholder text',
         sv: 'Placeholder text',
         en: 'Placeholder text',
@@ -408,7 +631,12 @@ const getResultData = () => [
       },
     },
     courseCompletionMethodOther: {
-      all: {
+      isSelected: {
+        fi: 'Olet valinnut kurssillesi suoritusmuodoksi muun tavan.',
+        sv: 'Olet valinnut kurssillesi suoritusmuodoksi muun tavan.',
+        en: 'Olet valinnut kurssillesi suoritusmuodoksi muun tavan.',
+      },
+      allDimensions: {
         fi: 'Placeholder text',
         sv: 'Placeholder text',
         en: 'Placeholder text',
@@ -445,7 +673,12 @@ const getResultData = () => [
       },
     },
     courseIsOpenUniversity: {
-      all: {
+      isSelected: {
+        fi: 'Osallistuminen kurssillesi on avointa kaikille.',
+        sv: 'Osallistuminen kurssillesi on avointa kaikille.',
+        en: 'Osallistuminen kurssillesi on avointa kaikille.',
+      },
+      allDimensions: {
         fi: 'Placeholder text',
         sv: 'Placeholder text',
         en: 'Placeholder text',
@@ -482,7 +715,12 @@ const getResultData = () => [
       },
     },
     courseIsNotOpenUniversity: {
-      all: {
+      isSelected: {
+        fi: 'Osallistuminen kurssillesi ei ole avointa kaikille.',
+        sv: 'Osallistuminen kurssillesi ei ole avointa kaikille.',
+        en: 'Osallistuminen kurssillesi ei ole avointa kaikille.',
+      },
+      allDimensions: {
         fi: 'Placeholder text',
         sv: 'Placeholder text',
         en: 'Placeholder text',
