df2c9cf0ea0a7e09306035987765d8bb54032940
Henri Remonen <henri@remonen.fi>
2023-02-14 10:38:13 +0200

    Construct new object base on the form data, this object is used to give the results

--- src/client/components/ResultPage/Results.tsx
+++ src/client/components/ResultPage/Results.tsx
@@ -4,29 +4,43 @@ import { Box, Container, Typography } from '@mui/material'
 import getResultData from '../../../server/db/seeders/data/devResultData'
 import styles from './styles'
 import { FormValues } from '../../types'
+/* eslint-disable @typescript-eslint/no-unused-vars */
 
 const Results = ({ formResultData }: { formResultData: FormValues }) => {
   const { t } = useTranslation()
 
   if (!formResultData) return null
 
+  const dimensionsId = 1
+  const completionId = 4
+
   const classes = styles.cardStyles
 
   const resultData = getResultData()
 
-  console.log(resultData)
+  const allDimensions: boolean = Object.values(
+    formResultData[dimensionsId]
+  ).every((dimension) => dimension)
 
-  console.log(formResultData)
+  const selectedCompletionMethods = Object.keys(
+    formResultData[completionId]
+  ).filter((method) => formResultData[completionId][method])
 
-  const allDimensions: boolean = Object.values(formResultData[1]).every(
-    (dimension) => dimension
-  )
+  const resultObject = {
+    ...formResultData,
+    1: allDimensions
+      ? 'all'
+      : Object.keys(formResultData[dimensionsId]).filter(
+          (dimension) => formResultData[dimensionsId][dimension]
+        ),
+    4: selectedCompletionMethods,
+  }
 
-  console.log(allDimensions)
+  console.log('RESULTS:', resultObject)
 
   return (
     <Box sx={{ m: 2, maxWidth: 1080, border: 1, borderColor: 'grey.300' }}>
-      <Container sx={classes.resultContainer}>
+      <Container sx={{ mt: 2 }}>
         <Typography variant="h5" sx={classes.heading} component="div">
           {t('results:title')}
         </Typography>
@@ -34,6 +48,16 @@ const Results = ({ formResultData }: { formResultData: FormValues }) => {
           <Typography variant="body2">{t('results:resultsMessage')}</Typography>
         </Box>
       </Container>
+
+      {Object.values(formResultData)
+        .slice(1)
+        .map((result: any) => (
+          <Container sx={classes.resultContainer}>
+            <Typography variant="h5" sx={classes.heading} component="div">
+              {resultData[result]?.isSelected.fi}
+            </Typography>
+          </Container>
+        ))}
     </Box>
   )
 }
