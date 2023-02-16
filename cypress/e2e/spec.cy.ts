58c6aa67881dd2f762b16606a167b31a698ac41e
Henri Remonen <henri@remonen.fi>
2023-02-16 10:05:08 +0200

    Refactor duplicative code into beforeEach block

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -6,30 +6,24 @@ import { Question } from '../../src/client/types'
 describe('Kliksutin web page', () => {
   let questionData
 
-  it('loads the main page', () => {
-    cy.visit(baseUrl)
-
-    cy.contains('Kliksutin')
-  })
-
-  it('user must proceed after faculty and dimensions are selected', () => {
-    cy.visit(baseUrl)
-
-    cy.get(`[data-cy = "faculty-select"]`).click()
-    cy.get(`[data-cy = "faculty-option-Faculty of Science"]`)
-
-    cy.get(`[data-cy = "dimension-select-collaboration"]`)
-
-    cy.get('#open-form-button')
-  })
-
-  it('loads visible questions correctly', () => {
+  beforeEach(() => {
     cy.visit(baseUrl)
 
     cy.get(`[data-cy = "faculty-select"]`).click()
     cy.get(`[data-cy = "faculty-option-Faculty of Science"]`).click()
 
     cy.get(`[data-cy = "dimension-select-collaboration"]`).click()
+  })
+
+  it('loads the main page', () => {
+    cy.contains('Kliksutin')
+  })
+
+  it('user must proceed after faculty and dimensions are selected', () => {
+    cy.get('#open-form-button')
+  })
+
+  it('loads visible questions correctly', () => {
     cy.get(`[data-cy = "dimension-select-discussion"]`).click()
 
     cy.get('#open-form-button').click()
@@ -44,8 +38,6 @@ describe('Kliksutin web page', () => {
   })
 
   it('does not load invisible questions before they have been touched', () => {
-    cy.visit(baseUrl)
-
     questionData = getQuestionData()
 
     cy.wrap(questionData).each((question: Question) => {
@@ -56,8 +48,6 @@ describe('Kliksutin web page', () => {
   })
 
   it.skip('has spot for recommendations', () => {
-    cy.visit(baseUrl)
-
     cy.contains('Tools that support your selections')
   })
 })
