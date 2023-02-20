6f25cef86d8fbaba5d6834cafdde139be31298fd
Veikkosuhonen <veikko.suhonen@helsinki.fi>
2023-02-20 17:56:28 +0200

    Better hy style shapes

--- src/client/theme.ts
+++ src/client/theme.ts
@@ -4,31 +4,63 @@ import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 
 import { useMemo } from 'react'
-import { createTheme, responsiveFontSizes } from '@mui/material/styles'
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeOptions,
+} from '@mui/material/styles'
+
+const themeOptions: ThemeOptions = {
+  typography: {
+    fontFamily: [
+      '"Open Sans"',
+      '"Helvetica"',
+      '"Arial"',
+      '"sans-serif"',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+
+  palette: {
+    primary: {
+      main: '#107eab',
+    },
+  },
+
+  shape: {
+    borderRadius: 0,
+  },
+
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: 0,
+        },
+        outlined: {
+          borderWidth: '2px',
+          ':hover': {
+            borderWidth: '2px',
+          },
+        },
+      },
+    },
+
+    MuiChip: {
+      styleOverrides: {
+        root: {
+          borderRadius: 5,
+        },
+      },
+    },
+  },
+}
 
 const useTheme = () => {
   const theme = useMemo(
-    () =>
-      responsiveFontSizes(
-        createTheme({
-          typography: {
-            fontFamily: [
-              '"Open Sans"',
-              '"Helvetica"',
-              '"Arial"',
-              '"sans-serif"',
-              '"Apple Color Emoji"',
-              '"Segoe UI Emoji"',
-              '"Segoe UI Symbol"',
-            ].join(','),
-          },
-          palette: {
-            primary: {
-              main: '#107eab',
-            },
-          },
-        })
-      ),
+    () => responsiveFontSizes(createTheme(themeOptions)),
     []
   )
 
