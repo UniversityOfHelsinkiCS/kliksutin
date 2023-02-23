f970230d3f389db703aa5cf08e1192b082bbbf0d
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-02-23 08:57:02 +0200

    Setup react query

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { QueryClientProvider } from 'react-query'
 import CssBaseline from '@mui/material/CssBaseline'
 
 import { PUBLIC_URL } from '../config'
+import queryClient from './util/queryClient'
 import App from './App'
 import initializeSentry from './util/sentry'
 import initializeI18n from './util/il18n'
@@ -15,9 +17,11 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <BrowserRouter basename={PUBLIC_URL}>
-      <CssBaseline>
-        <App />
-      </CssBaseline>
+      <QueryClientProvider client={queryClient}>
+        <CssBaseline>
+          <App />
+        </CssBaseline>
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
