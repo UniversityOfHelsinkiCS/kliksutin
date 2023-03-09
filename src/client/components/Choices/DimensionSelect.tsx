520fc9ff08a93619938c2c8ea0682f1a202c6113
Henri Remonen <henri@remonen.fi>
2023-03-09 08:53:44 +0200

    Created tooltip for dimension selections to elaborate on the dimension text

--- src/client/components/Choices/DimensionSelect.tsx
+++ src/client/components/Choices/DimensionSelect.tsx
@@ -1,51 +1,63 @@
 import React from 'react'
 import { Controller } from 'react-hook-form'
-import { Box, FormControl, Checkbox } from '@mui/material'
-import { InputProps, DimensionSelectionData } from '../../types'
+import { useTranslation } from 'react-i18next'
+import { Box, FormControl, Checkbox, Tooltip } from '@mui/material'
+import { InputProps, DimensionSelectionData, Locales } from '../../types'
 import DimensionChip from '../Chip/DimensionChip'
 import colors from '../../util/colors'
 
-const DimensionSelect = ({ control, question }: InputProps) => (
-  <>
-    {(question.optionData.options as DimensionSelectionData[]).map(
-      (choice: DimensionSelectionData) => (
-        <Controller
-          key={choice.id}
-          name={`${question.id}.${choice.id}`}
-          control={control}
-          defaultValue={false}
-          render={({ field }) => (
-            <FormControl sx={{ width: '100%' }}>
-              <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.3 }}>
-                <Checkbox
-                  {...field}
-                  data-cy={`dimension-select-${choice.id}`}
-                  icon={
-                    <DimensionChip
-                      key={choice.id}
-                      choice={choice}
-                      color={undefined}
-                      compact={false}
-                    />
-                  }
-                  checkedIcon={
-                    <DimensionChip
-                      key={choice.id}
-                      choice={choice}
-                      color={colors[choice.id]}
-                      compact={false}
-                    />
-                  }
-                  value={choice.id}
-                  checked={field.value}
-                />
-              </Box>
-            </FormControl>
-          )}
-        />
-      )
-    )}
-  </>
-)
+const DimensionSelect = ({ control, question }: InputProps) => {
+  const { i18n } = useTranslation()
+  const { language } = i18n
+
+  console.log(language)
+
+  return (
+    <>
+      {(question.optionData.options as DimensionSelectionData[]).map(
+        (choice: DimensionSelectionData) => (
+          <Controller
+            key={choice.id}
+            name={`${question.id}.${choice.id}`}
+            control={control}
+            defaultValue={false}
+            render={({ field }) => (
+              <FormControl sx={{ width: '100%' }}>
+                <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.3 }}>
+                  <Tooltip title={choice.text[language as keyof Locales]} arrow>
+                    <div>
+                      <Checkbox
+                        {...field}
+                        data-cy={`dimension-select-${choice.id}`}
+                        icon={
+                          <DimensionChip
+                            key={choice.id}
+                            choice={choice}
+                            color={undefined}
+                            compact={false}
+                          />
+                        }
+                        checkedIcon={
+                          <DimensionChip
+                            key={choice.id}
+                            choice={choice}
+                            color={colors[choice.id]}
+                            compact={false}
+                          />
+                        }
+                        value={choice.id}
+                        checked={field.value}
+                      />
+                    </div>
+                  </Tooltip>
+                </Box>
+              </FormControl>
+            )}
+          />
+        )
+      )}
+    </>
+  )
+}
 
 export default DimensionSelect
--- src/client/types.ts
+++ src/client/types.ts
@@ -48,6 +48,7 @@ export interface ToolType {
 export interface DimensionSelectionData {
   id: string
   label: Locales
+  text: Locales
   data: ToolType[]
 }
 
