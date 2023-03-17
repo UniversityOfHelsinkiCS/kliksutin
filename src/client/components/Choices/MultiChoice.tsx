111740df2131d344b8e0b0db4b2b59b0b1de3c2f
Henri Remonen <henri@remonen.fi>
2023-03-17 12:16:33 +0200

    Remove styles file from the choice component folder, use the global file instead

--- src/client/components/Choices/MultiChoice.tsx
+++ src/client/components/Choices/MultiChoice.tsx
@@ -3,41 +3,46 @@ import { Controller } from 'react-hook-form'
 import { Box, FormControl, Checkbox, FormControlLabel } from '@mui/material'
 import ShowMore from '../Common/ShowMore'
 import { MultipleChoiceType, InputProps, Locales } from '../../types'
+import styles from '../../styles'
 
-const MultiChoice = ({ control, question, children, language }: InputProps) => (
-  <>
-    {(question.optionData.options as MultipleChoiceType[]).map(
-      (choice: MultipleChoiceType) => (
-        <Controller
-          key={choice.id}
-          name={`${question.id}.${choice.id}`}
-          control={control}
-          defaultValue={false}
-          render={({ field }) => (
-            <FormControl sx={{ width: '100%' }}>
-              <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.3 }}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      {...field}
-                      value={choice.id}
-                      checked={field.value}
-                    />
-                  }
-                  label={choice.label[language as keyof Locales]}
-                />
-                {choice.data ? (
-                  <ShowMore text={choice.data[language as keyof Locales]} />
-                ) : null}
-              </Box>
-            </FormControl>
-          )}
-        />
-      )
-    )}
+const MultiChoice = ({ control, question, children, language }: InputProps) => {
+  const { formStyles } = styles
 
-    {children}
-  </>
-)
+  return (
+    <>
+      {(question.optionData.options as MultipleChoiceType[]).map(
+        (choice: MultipleChoiceType) => (
+          <Controller
+            key={choice.id}
+            name={`${question.id}.${choice.id}`}
+            control={control}
+            defaultValue={false}
+            render={({ field }) => (
+              <FormControl sx={formStyles.formControl}>
+                <Box sx={formStyles.choiceBox}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        {...field}
+                        value={choice.id}
+                        checked={field.value}
+                      />
+                    }
+                    label={choice.label[language as keyof Locales]}
+                  />
+                  {choice.data ? (
+                    <ShowMore text={choice.data[language as keyof Locales]} />
+                  ) : null}
+                </Box>
+              </FormControl>
+            )}
+          />
+        )
+      )}
+
+      {children}
+    </>
+  )
+}
 
 export default MultiChoice
