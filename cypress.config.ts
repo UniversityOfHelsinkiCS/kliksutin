371aa782926889a00ce157aa920255ec0ff72f19
Henri Remonen <henri@remonen.fi>
2023-03-30 15:32:02 +0300

    More timeOut time for tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  defaultCommandTimeout: 10000,
+  viewportWidth: 1280,
+  viewportHeight: 720,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
