c99e49fa264bc4837bdaf6892725abfd444abddc
Henri Remonen <henri@remonen.fi>
2023-04-18 08:56:57 +0300

    Created public path

--- src/client/Router.tsx
+++ src/client/Router.tsx
@@ -8,6 +8,7 @@ import InteractiveForm from './components/InteractiveForm/InteractiveForm'
 const Router = () => (
   <Routes>
     <Route path="/*" element={<InteractiveForm />} />
+    <Route path="/public" element={<InteractiveForm />} />
     <Route path="/admin" element={<Admin />} />
     <Route path="/contact" element={<Contact />} />
   </Routes>
