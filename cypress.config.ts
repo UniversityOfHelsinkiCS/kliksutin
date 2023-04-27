c5479a0c62bfa7ef85425c1ddb696e2250b1eff9
Henri Remonen <henri@remonen.fi>
2023-04-27 12:59:03 +0300

    Increase cypress timeout

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  defaultCommandTimeout: 10000,
+  defaultCommandTimeout: 60000,
   viewportWidth: 1280,
   viewportHeight: 720,
   e2e: {
