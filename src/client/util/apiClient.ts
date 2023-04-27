436f9e94fd69c4af65d71e12f9749f1d8d299e2e
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-04-27 14:58:54 +0300

    Remove commented out code

--- src/client/util/apiClient.ts
+++ src/client/util/apiClient.ts
@@ -3,13 +3,11 @@ import axios from 'axios'
 import { PUBLIC_URL } from '../../config'
 
 const getApiClient = () => {
-  /* const isPublicVersion = window.location.href.includes('/public')
+  const isPublicVersion = window.location.href.includes('/public')
 
   const baseURL = isPublicVersion
     ? `${PUBLIC_URL}/api`
-    : `${PUBLIC_URL}/public/api` */
-
-  const baseURL = `${PUBLIC_URL}/api`
+    : `${PUBLIC_URL}/public/api`
 
   const apiClient = axios.create({ baseURL })
 
