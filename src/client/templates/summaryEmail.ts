a909322c6f74df2beffcfdf6e8743bf71fdda21d
Henri Remonen <henri@remonen.fi>
2023-04-27 09:34:23 +0300

    Crate html template string for the summary email

---
+++ src/client/templates/summaryEmail.ts
@@ -0,0 +1,81 @@
+const summaryEmailHTML = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta
+      name="description"
+      content="Digipedakuraattori"
+    />
+    <title>Curre summary email</title>
+  </head>
+  <body>
+    <div>
+      <h3>
+        <strong>
+          Curren viesti opettajalle
+        </strong>
+      </h3>
+      <p>
+        &nbsp;
+      </p>
+      <p>
+        Hei
+      </p>
+      <p>
+        Kiitos Curren k&auml;ytt&auml;misest&auml; opetuksen suunnitteluun. Alta n&auml;et kurssillesi tekem&auml;si pedagogiset valinnat ja niit&auml; tukevat Helsingin yliopiston tarjoamat sovellukset. Voit nyt jatkaa suunnittelua perehtym&auml;ll&auml; v&auml;lineisiin tarkemmin opetusty&ouml;n ohjeiden avulla ja olemalla lis&auml;ksi tarvittaessa yhteydess&auml; opetusteknologian k&auml;yt&ouml;n tukipalveluihin. Kurssin arvioinnissa k&auml;ytet&auml;&auml;n Norppa-palautej&auml;rjestelm&auml;&auml;, jonne voit my&ouml;s lis&auml;t&auml; omia kysymyksi&auml;si. Linkit mainittuihin palveluihin l&ouml;yd&auml;t alta.
+      </p>
+      <p>
+        &nbsp;
+      </p>
+      <p>
+        Hyv&auml;&auml; jatkoa suunnitteluun! Olethan yhteydess&auml; opiskelijoihin ja tarpeen mukaan tukipalveluihin hyviss&auml; ajoin ennen kurssin alkamista.
+      </p>
+      <p>
+        &nbsp;
+      </p>
+      <p>
+        *********
+      </p>
+      <p>
+        Opetusty&ouml;n ohjeet:
+        <a href="https://teaching.helsinki.fi/">
+          https://teaching.helsinki.fi/
+        </a>
+      </p>
+      <p>
+        Moodle-aiheiset palvelupyynn&ouml;t:
+        <a href="mailto:moodle@helsinki.fi">
+          moodle@helsinki.fi
+        </a>
+      </p>
+      <p>
+        Muut opetusteknologiaan liittyv&auml;t palvelupyynn&ouml;t:
+        <a href="mailto:opetusteknologia@helsinki.fi">
+          opetusteknologia@helsinki.fi
+        </a>
+      </p>
+      <p>
+        Norppa-kurssipalautej&auml;rjestelm&auml;:
+        <a href="http://coursefeedback.helsinki.fi/">
+          http://coursefeedback.helsinki.fi/
+        </a>
+      </p>
+      <p>
+        &nbsp;
+      </p>
+      <p>
+        **********
+      </p>
+      <p>
+        <strong>
+          Kooste Curressa tekemist&auml;si valinnoista ja k&auml;ytett&auml;viss&auml; olevista sovelluksista:
+        </strong>
+      </p>  
+    </div>
+  </body>
+</html>
+`
+
+export default summaryEmailHTML
