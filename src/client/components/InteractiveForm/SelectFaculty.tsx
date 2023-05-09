b41bd52d93144e0d63000031ab50b07880253e5c
Henri Remonen <henri@remonen.fi>
2023-05-09 10:52:14 +0300

    #123 Add info box below faculty selection

--- src/client/components/InteractiveForm/SelectFaculty.tsx
+++ src/client/components/InteractiveForm/SelectFaculty.tsx
@@ -1,17 +1,22 @@
-import React, { useState } from 'react'
-import InputLabel from '@mui/material/InputLabel'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useEffect, useState } from 'react'
+import Select from '@mui/material/Select'
 import MenuItem from '@mui/material/MenuItem'
+import InputLabel from '@mui/material/InputLabel'
 import FormControl from '@mui/material/FormControl'
-import Select, { SelectChangeEvent } from '@mui/material/Select'
 import { Box, Typography } from '@mui/material'
 import { Controller } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
-import styles from '../../styles'
-import { InputProps, Faculty, Locales } from '../../types'
+
 import useFaculties from '../../hooks/useFaculties'
 import useUserFaculties from '../../hooks/useUserFaculties'
 import Markdown from '../Common/Markdown'
-import extraOrganisations from '../../util/organisations'
+import ShowMore from '../Common/ShowMore'
+
+import { extraOrganisations, organisationInfos } from '../../util/organisations'
+
+import styles from '../../styles'
+import { InputProps, Faculty, Locales } from '../../types'
 
 const sortFaculties = (faculties: Faculty[], language: keyof Locales) => {
   const sortedFaculties = faculties.sort((a, b) => {
@@ -24,18 +29,40 @@ const sortFaculties = (faculties: Faculty[], language: keyof Locales) => {
   return sortedFaculties
 }
 
+const { cardStyles, formStyles } = styles
+
+const FacultyInfo = ({ faculty }: { faculty: Faculty }) => {
+  const { t, i18n } = useTranslation()
+  const { language } = i18n
+
+  const facultyInfo = organisationInfos.find(
+    (info) => info.code === faculty?.code
+  )
+
+  if (!facultyInfo) return null
+
+  return (
+    <Box sx={cardStyles.content}>
+      <Typography component="div">
+        {t('facultySelect:infoMessage')}
+        <ShowMore text={facultyInfo.info[language as keyof Locales]} />
+      </Typography>
+    </Box>
+  )
+}
+
 const SelectFaculty = ({ control }: InputProps) => {
   const { t, i18n } = useTranslation()
   const { language } = i18n
-  const [faculty, setFaculty] = useState('')
+  const [faculty, setFaculty] = useState<Faculty>()
   const { faculties, isLoading: facultiesLoading } = useFaculties()
   const { userFaculties, isLoading: userFacultiesLoading } = useUserFaculties()
 
-  const handleChange = (event: SelectChangeEvent) => {
-    setFaculty(event.target.value)
-  }
+  useEffect(() => {
+    if (userFacultiesLoading) return
 
-  const { cardStyles, formStyles } = styles
+    setFaculty(userFaculties[0])
+  }, [userFaculties, userFacultiesLoading])
 
   if (facultiesLoading || userFacultiesLoading) return null
 
@@ -61,9 +88,7 @@ const SelectFaculty = ({ control }: InputProps) => {
             <Select
               sx={cardStyles.inputField}
               data-cy="faculty-select"
-              value={faculty}
               label={t('facultySelect:inputLabel')}
-              onChange={handleChange}
               {...field}
             >
               {organisations.map((f: Faculty) => (
@@ -71,6 +96,7 @@ const SelectFaculty = ({ control }: InputProps) => {
                   data-cy={`faculty-option-${f.code}`}
                   key={f.code}
                   value={f.code}
+                  onClick={() => setFaculty(f)}
                 >
                   {f.name[language as keyof Locales]}
                 </MenuItem>
@@ -79,6 +105,7 @@ const SelectFaculty = ({ control }: InputProps) => {
           </FormControl>
         )}
       />
+      <FacultyInfo faculty={faculty} />
     </Box>
   )
 }
--- src/client/util/organisations.ts
+++ src/client/util/organisations.ts
@@ -23,113 +23,113 @@ export const organisationInfos = [
   {
     code: 'H57',
     info: {
-      fi: 'Bio- ja ympäristötieteellinen tiedekunta',
-      en: 'Faculty of Biological and Environmental Sciences',
-      sv: 'Bio- och miljövetenskapliga fakulteten',
+      fi: '',
+      en: '',
+      sv: '',
     },
   },
   {
     code: 'H90',
     info: {
-      fi: 'Eläinlääketieteellinen tiedekunta',
-      en: 'Faculty of Veterinary Medicine',
-      sv: 'Veterinärmedicinska fakulteten',
+      fi: '',
+      en: '',
+      sv: '',
     },
   },
   {
     code: 'H55',
     info: {
-      fi: 'Farmasian tiedekunta',
-      en: 'Faculty of Pharmacy',
-      sv: 'Farmaceutiska fakulteten',
+      fi: '',
+      en: '',
+      sv: '',
     },
   },
   {
     code: 'H40',
     info: {
-      fi: 'Humanistinen tiedekunta',
-      en: 'Faculty of Arts',
-      sv: 'Humanistiska fakulteten',
+      fi: '',
+      en: '',
+      sv: '',
     },
   },
   {
     code: 'H60',
     info: {
-      fi: 'Kasvatustieteellinen tiedekunta',
-      en: 'Faculty of Educational Sciences',
-      sv: 'Pedagogiska fakulteten',
+      fi: '',
+      en: '',
+      sv: '',
     },
   },
   {
     code: 'H30',
     info: {
-      fi: 'Lääketieteellinen tiedekunta',
-      en: 'Faculty of Medicine',
-      sv: 'Medicinska fakulteten',
+      fi: '',
+      en: '',
+      sv: '',
     },
   },
   {
     code: 'H80',
     info: {
-      fi: 'Maatalous-metsätieteellinen tiedekunta',
-      en: 'Faculty of Agriculture and Forestry',
-      sv: 'Agrikultur-forstvetenskapliga fakulteten',
+      fi: '',
+      en: '',
+      sv: '',
     },
   },
   {
     code: 'H50',
     info: {
-      fi: 'Matemaattis-luonnontieteellinen tiedekunta',
-      en: 'Faculty of Science',
-      sv: 'Matematisk-naturvetenskapliga fakulteten',
+      fi: '',
+      en: '',
+      sv: '',
     },
   },
   {
     code: 'H20',
     info: {
-      fi: 'Oikeustieteellinen tiedekunta',
-      en: 'Faculty of Law',
-      sv: 'Juridiska fakulteten',
+      fi: '',
+      en: '',
+      sv: '',
     },
   },
   {
     code: 'H74',
     info: {
-      fi: 'Svenska social- och kommunalhögskolan',
-      en: 'Swedish School of Social Science',
-      sv: 'Svenska social- och kommunalhögskolan',
+      fi: '',
+      en: '',
+      sv: '',
     },
   },
   {
     code: 'H10',
     info: {
-      fi: 'Teologinen tiedekunta',
-      en: 'Faculty of Theology',
-      sv: 'Teologiska fakulteten',
+      fi: '',
+      en: '',
+      sv: '',
     },
   },
   {
     code: 'H70',
     info: {
-      fi: 'Valtiotieteellinen tiedekunta',
-      en: 'Faculty of Social Sciences',
-      sv: 'Statsvetenskapliga fakulteten',
+      fi: '',
+      en: '',
+      sv: '',
     },
   },
   {
     code: 'H906',
-    name: {
-      fi: 'Kielikeskus',
-      sv: 'Språkcentrum',
-      en: 'Language Centre',
+    info: {
+      fi: '',
+      en: '',
+      sv: '',
     },
   },
   {
     code: 'OTHER',
-    name: {
-      fi: 'Muu',
-      sv: 'Annan',
-      en: 'Other',
+    info: {
+      fi: '',
+      en: '',
+      sv: '',
     },
   },
 ]
