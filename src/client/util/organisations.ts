23ad6b10a722d2758b33a193602777c254a32fb1
Henri Remonen <henri@remonen.fi>
2023-05-09 10:23:29 +0300

    Added info data for organisations

--- src/client/util/organisations.ts
+++ src/client/util/organisations.ts
@@ -1,6 +1,6 @@
 import { Faculty } from '../types'
 
-const extraOrganisations: Faculty[] = [
+export const extraOrganisations: Faculty[] = [
   {
     code: 'H906',
     name: {
@@ -19,4 +19,117 @@ const extraOrganisations: Faculty[] = [
   },
 ]
 
-export default extraOrganisations
+export const organisationInfos = [
+  {
+    code: 'H57',
+    info: {
+      fi: 'Bio- ja ympäristötieteellinen tiedekunta',
+      en: 'Faculty of Biological and Environmental Sciences',
+      sv: 'Bio- och miljövetenskapliga fakulteten',
+    },
+  },
+  {
+    code: 'H90',
+    info: {
+      fi: 'Eläinlääketieteellinen tiedekunta',
+      en: 'Faculty of Veterinary Medicine',
+      sv: 'Veterinärmedicinska fakulteten',
+    },
+  },
+  {
+    code: 'H55',
+    info: {
+      fi: 'Farmasian tiedekunta',
+      en: 'Faculty of Pharmacy',
+      sv: 'Farmaceutiska fakulteten',
+    },
+  },
+  {
+    code: 'H40',
+    info: {
+      fi: 'Humanistinen tiedekunta',
+      en: 'Faculty of Arts',
+      sv: 'Humanistiska fakulteten',
+    },
+  },
+  {
+    code: 'H60',
+    info: {
+      fi: 'Kasvatustieteellinen tiedekunta',
+      en: 'Faculty of Educational Sciences',
+      sv: 'Pedagogiska fakulteten',
+    },
+  },
+  {
+    code: 'H30',
+    info: {
+      fi: 'Lääketieteellinen tiedekunta',
+      en: 'Faculty of Medicine',
+      sv: 'Medicinska fakulteten',
+    },
+  },
+  {
+    code: 'H80',
+    info: {
+      fi: 'Maatalous-metsätieteellinen tiedekunta',
+      en: 'Faculty of Agriculture and Forestry',
+      sv: 'Agrikultur-forstvetenskapliga fakulteten',
+    },
+  },
+  {
+    code: 'H50',
+    info: {
+      fi: 'Matemaattis-luonnontieteellinen tiedekunta',
+      en: 'Faculty of Science',
+      sv: 'Matematisk-naturvetenskapliga fakulteten',
+    },
+  },
+  {
+    code: 'H20',
+    info: {
+      fi: 'Oikeustieteellinen tiedekunta',
+      en: 'Faculty of Law',
+      sv: 'Juridiska fakulteten',
+    },
+  },
+  {
+    code: 'H74',
+    info: {
+      fi: 'Svenska social- och kommunalhögskolan',
+      en: 'Swedish School of Social Science',
+      sv: 'Svenska social- och kommunalhögskolan',
+    },
+  },
+  {
+    code: 'H10',
+    info: {
+      fi: 'Teologinen tiedekunta',
+      en: 'Faculty of Theology',
+      sv: 'Teologiska fakulteten',
+    },
+  },
+  {
+    code: 'H70',
+    info: {
+      fi: 'Valtiotieteellinen tiedekunta',
+      en: 'Faculty of Social Sciences',
+      sv: 'Statsvetenskapliga fakulteten',
+    },
+  },
+  {
+    code: 'H906',
+    name: {
+      fi: 'Kielikeskus',
+      sv: 'Språkcentrum',
+      en: 'Language Centre',
+    },
+  },
+  {
+    code: 'OTHER',
+    name: {
+      fi: 'Muu',
+      sv: 'Annan',
+      en: 'Other',
+    },
+  },
+]
