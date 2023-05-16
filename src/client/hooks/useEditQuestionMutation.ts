7a8977d9dacf27e844193f2957b51cb6db33b50c
Henri Remonen <henri@remonen.fi>
2023-05-16 10:02:45 +0300

    Invalidate questions when modifying question title or text

--- src/client/hooks/useEditQuestionMutation.ts
+++ src/client/hooks/useEditQuestionMutation.ts
@@ -2,6 +2,8 @@ import { useMutation } from 'react-query'
 import { Locales } from '../types'
 
 import apiClient from '../util/apiClient'
+import queryClient from '../util/queryClient'
+import useSurvey from './useSurvey'
 
 type QuestionsUpdates = {
   title: Locales
@@ -9,11 +11,16 @@ type QuestionsUpdates = {
 }
 
 const useEditQuestionMutation = (questionId: number) => {
+  const { survey } = useSurvey()
+
   const mutationFn = async (data: QuestionsUpdates) => {
     await apiClient.put(`/questions/${questionId}`, data)
   }
 
-  const mutation = useMutation(mutationFn)
+  const mutation = useMutation(mutationFn, {
+    onSuccess: () =>
+      queryClient.invalidateQueries({ queryKey: ['questions', survey.id] }),
+  })
 
   return mutation
 }
