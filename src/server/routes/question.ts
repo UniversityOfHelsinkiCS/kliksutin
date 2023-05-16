ab15cd730ec63c0709c8d2e4e7c9e5492a2b0228
Henri Remonen <henri@remonen.fi>
2023-05-16 09:12:17 +0300

    Endpoint for option modification

--- src/server/routes/question.ts
+++ src/server/routes/question.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 
-import { RequestWithUser } from '../types'
+import { QuestionsUpdates, RequestWithUser } from '../types'
 import { Question } from '../db/models'
 
 const questionRouter = express.Router()
@@ -27,15 +27,43 @@ questionRouter.put('/:id', async (req: RequestWithUser, res) => {
 
   if (!question) throw new Error('Question not found')
 
-  const updates = req.body
+  const updates: QuestionsUpdates = req.body
 
   Object.assign(question, updates)
-
-  console.log(question)
 
   await question.save()
 
   return res.send(question)
 })
+
+questionRouter.put(
+  '/:id/option/:optionId',
+  async (req: RequestWithUser, res) => {
+    const { id, optionId } = req.params
+    const { isAdmin } = req.user
+
+    if (!isAdmin) throw new Error('Unauthorized')
+
+    const question = await Question.findByPk(id)
+
+    if (!question) throw new Error('Question not found')
+
+    const option = question.optionData.options.find(
+      (aOption) => aOption.id === optionId
+    )
+
+    if (!option) throw new Error('Option not found')
+
+    const updates = question.optionData.options.map((aOption) =>
+      aOption.id === optionId ? Object.assign(option, req.body) : aOption
+    )
+
+    Object.assign(question, updates)
+
+    await question.save()
+
+    return res.send(question)
+  }
+)
 
 export default questionRouter
