a4106af14b0335ae0b7fc1c0973e4b7b027948b3
Henri Remonen <henri@remonen.fi>
2023-05-16 10:01:48 +0300

    Create react query hook for fetching questiondata

---
+++ src/client/hooks/useQuestions.ts
@@ -0,0 +1,22 @@
+import { useQuery } from 'react-query'
+
+import apiClient from '../util/apiClient'
+import { Question } from '../types'
+
+const useQuestions = (surveyId: number) => {
+  const queryKey = ['questions', surveyId]
+
+  const query = async (): Promise<Question[]> => {
+    const { data } = await apiClient.get(`/questions/${surveyId}`)
+
+    return data
+  }
+
+  const { data: questions, ...rest } = useQuery(queryKey, query, {
+    enabled: Boolean(surveyId),
+  })
+
+  return { questions, ...rest }
+}
+
+export default useQuestions
