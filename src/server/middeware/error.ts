fe3e5eaa5016cd646b5261404d556a4b28158f81
Henri Remonen <henri@remonen.fi>
2023-05-25 09:26:10 +0300

    Error handler typing

--- src/server/middeware/error.ts
+++ src/server/middeware/error.ts
@@ -1,8 +1,15 @@
+import { Request, Response, NextFunction } from 'express'
+
 import Sentry from '@sentry/node'
 
 import logger from '../util/logger'
 
-const errorHandler = (error: any, _req: any, _res: any, next: any) => {
+const errorHandler = (
+  error: Error,
+  _req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
   logger.error(`${error.message} ${error.name} ${error.stack}`)
 
   Sentry.captureException(error)
