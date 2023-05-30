b0d40773c6aea5399c97b2a61d383302f3101b8f
Henri Remonen <henri@remonen.fi>
2023-05-30 09:16:56 +0300

    Change the result validator to only validate single item

--- src/client/validators/results.ts
+++ src/client/validators/results.ts
@@ -1,14 +1,13 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { z } from 'zod'
 
-export const ResultZod = z.record(
-  z.object({
-    isSelected: z.object({
-      fi: z.string().nonempty(),
-      sv: z.string().nonempty(),
-      en: z.string().nonempty(),
-    }),
-  })
-)
+export const ResultZod = z.object({
+  optionLabel: z.string().nonempty(),
+  isSelected: z.object({
+    fi: z.string().nonempty(),
+    sv: z.string().nonempty(),
+    en: z.string().nonempty(),
+  }),
+})
 
 export type NewResult = z.infer<typeof ResultZod>
