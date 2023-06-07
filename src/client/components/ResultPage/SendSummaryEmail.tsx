9bc33506bbe8441bb546f3efd03b3592b640820c
Henri Remonen <henri@remonen.fi>
2023-06-07 08:37:38 +0300

    Update the summary email onSubmit handler

--- src/client/components/ResultPage/SendSummaryEmail.tsx
+++ src/client/components/ResultPage/SendSummaryEmail.tsx
@@ -39,7 +39,7 @@ const SendSummaryEmail = () => {
     })
   }
 
-  const onSubmit = async () => {
+  const onSubmit = () => {
     const targets = [user?.email]
     const text = `\
       ${summaryEmailHTML} \
@@ -62,7 +62,7 @@ const SendSummaryEmail = () => {
       ${resultHTML} \
       `
 
-    await sendResultsToEmail(targets, text)
+    sendResultsToEmail(targets, text)
       .then(() => setIsSent(true))
       .catch((err) => {
         console.log(err)
