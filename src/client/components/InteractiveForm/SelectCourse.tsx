dac0e450d41d2ac4dfd391d6cacf1964f3dde5bc
Henri Remonen <henri@remonen.fi>
2023-06-09 13:39:12 +0300

    Perhaps more robust course selection

--- src/client/components/InteractiveForm/SelectCourse.tsx
+++ src/client/components/InteractiveForm/SelectCourse.tsx
@@ -14,6 +14,14 @@ import { Course, InputProps, Locales } from '../../types'
 
 const sortCourses = (courses: Course[]) => {
   const sortedCourses = courses.sort((a, b) => {
+    if (
+      !a?.courseUnits ||
+      !b?.courseUnits ||
+      a.courseUnits.length === 0 ||
+      b.courseUnits.length === 0
+    )
+      return -1
+
     const aCode = a.courseUnits ? a.courseUnits[0]?.code : ''
     const bCode = b.courseUnits ? b.courseUnits[0]?.code : ''
     if (aCode > bCode) return 1
