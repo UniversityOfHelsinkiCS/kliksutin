16d92207258872c30b946e0be554d75f804083b6
Henri Remonen <henri@remonen.fi>
2023-07-03 13:42:35 +0300

    Create mutation hooks for the dimension stuff

---
+++ src/client/hooks/useDimensionMutation.ts
@@ -0,0 +1,62 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useMutation } from 'react-query'
+
+import useSurvey from './useSurvey'
+
+import { Locales } from '../types'
+import { NewDimension } from '../validators/options'
+
+import apiClient from '../util/apiClient'
+import queryClient from '../util/queryClient'
+
+type DimensionUpdates = {
+  title: Locales
+  text: Locales
+}
+
+export const useEditDimensionMutation = (dimensionId: string) => {
+  const { survey } = useSurvey()
+
+  const dimensionquestion = survey.Questions.find(
+    (question) => question.optionData.type === 'dimensions'
+  )
+
+  const mutationFn = async (data: DimensionUpdates) => {
+    await apiClient.put(
+      `/questions/${dimensionquestion.id}/option/${dimensionId}`,
+      data
+    )
+  }
+
+  const mutation = useMutation(mutationFn, {
+    onSuccess: () =>
+      queryClient.invalidateQueries({
+        queryKey: ['questions', survey.id],
+      }),
+  })
+
+  return mutation
+}
+
+export const useDeleteDimensionMutation = (dimensionId: string) => {
+  const { survey } = useSurvey()
+
+  const dimensionquestion = survey.Questions.find(
+    (question) => question.optionData.type === 'dimensions'
+  )
+
+  const mutationFn = async () => {
+    await apiClient.delete(
+      `/questions/${dimensionquestion.id}/option/${dimensionId}`
+    )
+  }
+
+  const mutation = useMutation(mutationFn, {
+    onSuccess: () =>
+      queryClient.invalidateQueries({
+        queryKey: ['questions', survey.id],
+      }),
+  })
+
+  return mutation
+}
