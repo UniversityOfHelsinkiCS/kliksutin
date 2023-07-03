3136c7994563f7dbbbe511b48b987aa0e67985f0
Henri Remonen <henri@remonen.fi>
2023-07-03 10:25:49 +0300

    Create a component for rendering the dimension edits'

--- src/client/Router.tsx
+++ src/client/Router.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import Admin from './components/Admin/Admin'
 import Contact from './components/ContactPage/Contact'
 import InteractiveForm from './components/InteractiveForm/InteractiveForm'
+import EditDimensions from './components/Admin/EditDimensions/EditDimensions'
 import EditQuestions from './components/Admin/EditQuestions/EditQuestions'
 import EditResults from './components/Admin/EditResults/EditResults'
 import EditRecommendations from './components/Admin/EditRecommendations/EditRecommendations'
@@ -13,7 +14,7 @@ const Router = () => (
     <Route path="/*" element={<InteractiveForm />} />
     <Route path="/public" element={<InteractiveForm />} />
     <Route path="/admin" element={<Admin />}>
-      <Route path="edit-dimensions" element={<> </>} />
+      <Route path="edit-dimensions" element={<EditDimensions />} />
       <Route path="edit-questions" element={<EditQuestions />} />
       <Route path="edit-results" element={<EditResults />} />
       <Route path="edit-recommendations" element={<EditRecommendations />} />
---
+++ src/client/components/Admin/EditDimensions/EditDimensions.tsx
@@ -0,0 +1,89 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState } from 'react'
+import { Box, Button, SelectChangeEvent, Typography } from '@mui/material'
+import { useTranslation } from 'react-i18next'
+
+import useSurvey from '../../../hooks/useSurvey'
+
+import { DimensionSelect, LanguageSelect } from '../Select'
+import EditDimension from './EditDimension'
+
+import { getDimensions } from '../../../util/dimensions'
+
+import { Locales } from '../../../types'
+
+const EditDimensions = () => {
+  const { t } = useTranslation()
+  const { survey } = useSurvey()
+
+  const [dimensionId, setDimensionId] = useState('')
+  const [selectedLanguage, setSelectedLanguage] = useState<keyof Locales>('en')
+
+  const [openNewQuestion, setOpenNewQuestion] = useState(false)
+
+  const handleDimensionChange = (event: SelectChangeEvent) => {
+    setDimensionId(event.target.value)
+  }
+
+  const handleLanguageChange = (event: SelectChangeEvent) => {
+    setSelectedLanguage(event.target.value as keyof Locales)
+  }
+
+  if (!survey) return null
+
+  const dimensions = getDimensions(survey)
+
+  const selectedDimension = dimensions.find(
+    (dimension) => dimension.id === dimensionId
+  )
+
+  return (
+    <Box sx={{ mx: 2, mt: 8 }}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          my: 4,
+          justifyContent: 'flex-start',
+        }}
+      >
+        <DimensionSelect
+          dimensionId={dimensionId}
+          dimensions={dimensions}
+          handleChange={handleDimensionChange}
+        />
+        <LanguageSelect
+          selectedLanguage={selectedLanguage}
+          handleChange={handleLanguageChange}
+        />
+        <Button
+          sx={{ position: 'absolute', right: 0, mr: 4, alignSelf: 'center' }}
+          variant="contained"
+          onClick={() => setOpenNewQuestion(!openNewQuestion)}
+        >
+          {t('admin:questionAddNew')}
+        </Button>
+      </Box>
+      <Box width="100%" flexWrap="wrap">
+        {selectedDimension ? (
+          <Box sx={{ my: 4 }}>
+            <Typography sx={{ my: 4, pl: 1 }} variant="h4">
+              {t('admin:questionViewQuestionEdit')}
+            </Typography>
+            <EditDimension
+              language={selectedLanguage}
+              dimension={selectedDimension}
+              onDelete={setDimensionId}
+            />
+          </Box>
+        ) : (
+          <Typography sx={{ my: 4, pl: 1 }} variant="h4">
+            {t('admin:questionViewInfo')}
+          </Typography>
+        )}
+      </Box>
+    </Box>
+  )
+}
+
+export default EditDimensions
