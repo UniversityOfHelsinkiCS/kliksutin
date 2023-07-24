2e69cdd4325d3a4e7dfc4d8494ecc06e4498a954
Henri Remonen <henri@remonen.fi>
2023-07-24 08:02:25 +0300

    Extract ResultElement component

--- src/client/components/ResultPage/Results.tsx
+++ src/client/components/ResultPage/Results.tsx
@@ -6,96 +6,19 @@ import { Box, Button, Container, Stack, Typography } from '@mui/material'
 import useSurvey from '../../hooks/useSurvey'
 import useResults from '../../hooks/useResults'
 
+import ResultElement from './ResultElement'
 import SendSummaryEmail from './SendSummaryEmail'
 import ProceedToContact from './ProceedToContact'
 import Openai from '../Openai/Openai'
-import Markdown from '../Common/Markdown'
 import ResetForm from '../Common/ResetForm'
 import CompactDimensionChips from '../Chip/CompactDimensionChips'
 
 import { getSelectedDimensions } from '../../util/dimensions'
 
 import styles from '../../styles'
-import {
-  DimensionSelectionData,
-  InputProps,
-  Locales,
-  Result,
-} from '../../types'
+import { InputProps, Locales } from '../../types'
 
 const { cardStyles, resultStyles, formStyles } = styles
-
-const ResultElement = ({
-  language,
-  resultData,
-  dimensionSelections,
-}: {
-  language: keyof Locales
-  resultData: Result | undefined
-  dimensionSelections: DimensionSelectionData[] | undefined | null
-}) => {
-  if (!resultData || !dimensionSelections) return null
-
-  const allDimensions: DimensionSelectionData = {
-    id: 'allDimensions',
-    label: 'allDimensions',
-    color: '#000000',
-    title: {
-      fi: 'Kaikki',
-      sv: 'All',
-      en: 'All',
-    },
-    text: {
-      fi: 'Kaikki',
-      sv: 'All',
-      en: 'All',
-    },
-  }
-
-  const dimensions = [allDimensions, ...dimensionSelections]
-
-  return (
-    <Container
-      style={{
-        margin: '2rem 0 2rem 0',
-        borderLeft: 'solid',
-        borderColor: '#9ca3af',
-        borderWidth: '1px',
-      }}
-    >
-      <Box style={{ margin: '2rem 0 2rem 1rem' }}>
-        <Markdown>{resultData.isSelected[language]}</Markdown>
-      </Box>
-      <Box
-        style={{
-          margin: '2rem 0 2rem 0',
-        }}
-      >
-        {dimensions.map((dimension) => {
-          const color = dimension.color ?? null
-
-          if (!resultData.data[dimension.id]) return null
-
-          return (
-            <Box
-              data-cy={`result-wrapper-${resultData.optionLabel}-${dimension.id}`}
-              key={`${JSON.stringify(resultData)}.${dimension.id}`}
-              style={{
-                margin: '1rem',
-                padding: '0 2rem 0 2rem ',
-                borderLeft: 'solid',
-                borderColor: color,
-                borderWidth: '6px',
-              }}
-            >
-              <Markdown>{resultData.data[dimension.id][language]}</Markdown>
-            </Box>
-          )
-        })}
-      </Box>
-    </Container>
-  )
-}
 
 const Results = ({
   formResultData,
