674f3582b03186e1552d20dcea9923e4b4ae0e9e
Henri Remonen <henri@remonen.fi>
2023-07-24 14:30:07 +0300

    Extract SelectCourse and SelectFaculty to own component

--- src/client/components/InteractiveForm/RenderSurvey.tsx
+++ src/client/components/InteractiveForm/RenderSurvey.tsx
@@ -1,23 +1,20 @@
 import React, { useState } from 'react'
-import { useLocation } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { Box, Button } from '@mui/material'
 
 import useSurvey from '../../hooks/useSurvey'
 
-import SelectCourse from './SelectCourse'
-import SelectFaculty from './SelectFaculty'
 import RenderQuestions from './RenderQuestions'
+import SurveyButtons from './SurveyButtons'
+import SurveySelections from './SurveySelections'
 
 import { getSelectedDimensionsFromWatch } from '../../util/dimensions'
 import { FORM_DATA_KEY } from '../../../config'
 
 import { InputProps } from '../../types'
 import styles from '../../styles'
-import SurveyButtons from './SurveyButtons'
 
 const RenderSurvey = ({ control, watch, isSubmitted }: InputProps) => {
-  const location = useLocation()
   const { t, i18n } = useTranslation()
   const { survey, isLoading } = useSurvey()
 
@@ -47,8 +44,8 @@ const RenderSurvey = ({ control, watch, isSubmitted }: InputProps) => {
 
   return (
     <Box sx={cardStyles.outerBox}>
-      <SelectFaculty control={control} />
-      {location.pathname !== '/public' && <SelectCourse control={control} />}
+      <SurveySelections control={control} />
+
       <Box sx={cardStyles.card}>
         {questions.map((question) => (
           <div key={question.id}>
---
+++ src/client/components/InteractiveForm/SurveySelections.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+import SelectCourse from './SelectCourse'
+import SelectFaculty from './SelectFaculty'
+
+import { InputProps } from '../../types'
+
+const SurveySelections = ({ control }: InputProps) => {
+  const location = useLocation()
+
+  return (
+    <Box>
+      <SelectFaculty control={control} />
+      {location.pathname !== '/public' && <SelectCourse control={control} />}
+    </Box>
+  )
+}
+
+export default SurveySelections
