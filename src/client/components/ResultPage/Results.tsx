a76a4de054f87749373e74e6bd8781b2d53fad37
Henri Remonen <henri@remonen.fi>
2023-07-24 14:14:31 +0300

    Refactor Results component

--- src/client/components/ResultPage/Results.tsx
+++ src/client/components/ResultPage/Results.tsx
@@ -3,13 +3,8 @@ import { useLocation } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { Box, Button, Container, Stack, Typography } from '@mui/material'
 
-import { Locales } from '@backend/types'
-
 import useSurvey from '../../hooks/useSurvey'
-import useResults from '../../hooks/useResults'
-import useResultRefCallback from '../../hooks/useResultRefCallback'
 
-import ResultElement from './ResultElement'
 import SendSummaryEmail from './SendSummaryEmail'
 import ProceedToContact from './ProceedToContact'
 import Openai from '../Openai/Openai'
@@ -18,55 +13,31 @@ import CompactDimensionChips from '../Chip/CompactDimensionChips'
 
 import { useResultData } from '../../contexts/ResultDataContext'
 
+import { getResultArray } from '../../util/results'
 import { getSelectedDimensionsFromResultData } from '../../util/dimensions'
 import styles from '../../styles'
-import { InputProps } from '../../types'
+import RenderResults from './RenderResults'
 
 const { cardStyles, resultStyles, formStyles } = styles
 
-const Results = ({ setShowResults }: InputProps & { setShowResults: any }) => {
+const Results = ({
+  setShowResults,
+}: {
+  setShowResults: React.Dispatch<React.SetStateAction<boolean>>
+}) => {
   const location = useLocation()
-  const { t, i18n } = useTranslation()
+  const { t } = useTranslation()
   const { survey } = useSurvey()
-  const { results, isSuccess: resultsFetched } = useResults(survey?.id)
-  const refCallback = useResultRefCallback()
-
   const { resultData } = useResultData()
-  const { language } = i18n
 
-  if (!survey || !resultsFetched || !resultData) return null
+  if (!survey || !resultData) return null
 
   const dimensionSelections = getSelectedDimensionsFromResultData(
     survey,
     resultData
   )
 
-  const objectToArray = (aChoiceId: number): string[] =>
-    Object.keys(resultData[aChoiceId]).filter(
-      (index) => resultData[aChoiceId][index]
-    )
-
-  const resultObjectsToArrays = (): string[][] => {
-    const entries = Object.entries(resultData)
-    return entries.map(([key, value]) => {
-      if (typeof value === 'object') {
-        const selections = objectToArray(Number(key))
-
-        // Hacky way: if the courseattendance is considered to be hybrid
-        // eg. the courseAttendancePresent and courseAttendanceRemote are both selected
-        if (
-          selections.includes('courseAttendancePresent') &&
-          selections.length === 2
-        ) {
-          return ['courseAttendanceHybrid']
-        }
-        return objectToArray(Number(key))
-      }
-      return [value]
-    })
-  }
-
-  const resultArrays = resultObjectsToArrays()
+  const resultArrays = getResultArray(resultData)
 
   // Dimensions are the first of the selections
   const dimensions = resultArrays[0]
@@ -102,31 +73,16 @@ const Results = ({ setShowResults }: InputProps & { setShowResults: any }) => {
             />
           </Container>
 
-          <Box ref={refCallback}>
-            {resultArray.map((resultLabels) =>
-              resultLabels.map((resultLabel) => (
-                <ResultElement
-                  key={JSON.stringify(resultLabel)}
-                  language={language as keyof Locales}
-                  resultData={results?.find(
-                    (result: { optionLabel: string }) =>
-                      result.optionLabel === resultLabel
-                  )}
-                  dimensionSelections={dimensionSelections}
-                />
-              ))
-            )}
-          </Box>
+          <RenderResults
+            resultArray={resultArray}
+            dimensionSelections={dimensionSelections}
+          />
 
           <SendSummaryEmail />
 
           <Box sx={formStyles.stackBoxWrapper}>
             <Stack sx={formStyles.stack} direction="row">
-              <Button
-                data-cy="back-to-selections"
-                sx={{ m: 1 }}
-                onClick={onNavigateBack}
-              >
+              <Button data-cy="back-to-selections" onClick={onNavigateBack}>
                 {'<'} {t('results:backToMessage')}
               </Button>
 
