2094aeca76cf422101295eb2b1e4dda87e839856
Henri Remonen <henri@remonen.fi>
2023-07-25 09:28:08 +0300

    Create route for getting all the entries

--- src/server/routes/entry.ts
+++ src/server/routes/entry.ts
@@ -5,6 +5,18 @@ import { Entry } from '../db/models'
 
 const entryRouter = express.Router()
 
+entryRouter.get('/:surveyId', async (req, res) => {
+  const { surveyId } = req.params
+
+  const entries = await Entry.findAll({
+    where: {
+      surveyId: Number(surveyId),
+    },
+  })
+
+  return res.status(200).send(entries)
+})
+
 entryRouter.post('/:surveyId', async (req: RequestWithUser, res: any) => {
   const { surveyId } = req.params
   const { id: userId } = req.user
