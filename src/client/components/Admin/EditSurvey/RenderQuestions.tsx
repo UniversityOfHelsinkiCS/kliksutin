e6b2d0d7351bf3b839e148f6def0fbd2e3a071d3
Henri Remonen <henri@remonen.fi>
2023-07-26 09:34:13 +0300

    Extract Question item component

--- src/client/components/Admin/EditSurvey/RenderQuestions.tsx
+++ src/client/components/Admin/EditSurvey/RenderQuestions.tsx
@@ -2,11 +2,58 @@ import React from 'react'
 import { Box, InputLabel, Typography } from '@mui/material'
 import { Locales, Question } from '@backend/types'
 
+interface QuestionsProps {
+  question: Question
+  language: keyof Locales
+}
+
 interface RenderQuestionsProps {
   question: Question
   questions: Question[]
   language: keyof Locales
 }
+
+const QuestionItem = ({ question, language }: QuestionsProps) => (
+  <Box
+    key={question.id}
+    sx={{
+      p: 2,
+      my: 4,
+      border: 1,
+      borderColor: 'grey.400',
+      position: 'relative',
+      '&:hover': {
+        border: 1,
+        borderColor: '#0288d1',
+      },
+    }}
+  >
+    <InputLabel
+      sx={{
+        mt: '-1.75em',
+        px: '0.5em',
+        zIndex: 2,
+        width: 'full',
+        backgroundColor: 'white',
+        position: 'absolute',
+      }}
+    >
+      {question.title[language]}
+    </InputLabel>
+    <Box>
+      <Typography variant="body2">ID: {question.id}</Typography>
+      <Typography variant="body2">
+        Järjestysnumero: {question.priority}
+      </Typography>
+      <Typography variant="body2">
+        Isäntäkysymys: {question.parentId}
+      </Typography>
+      <Typography variant="body2">
+        Sisältö: {question.text[language]}
+      </Typography>
+    </Box>
+  </Box>
+)
 
 const RenderQuestions = ({
   question,
@@ -21,45 +68,7 @@ const RenderQuestions = ({
 
   return (
     <Box sx={{ ml: 4 }}>
-      <Box
-        key={question.id}
-        sx={{
-          p: 2,
-          my: 4,
-          border: 1,
-          borderColor: 'grey.400',
-          position: 'relative',
-          '&:hover': {
-            border: 1,
-            borderColor: '#0288d1',
-          },
-        }}
-      >
-        <InputLabel
-          sx={{
-            mt: '-1.75em',
-            px: '0.5em',
-            zIndex: 2,
-            width: 'full',
-            backgroundColor: 'white',
-            position: 'absolute',
-          }}
-        >
-          {question.title[language]}
-        </InputLabel>
-        <Box>
-          <Typography variant="body2">ID: {question.id}</Typography>
-          <Typography variant="body2">
-            Järjestysnumero: {question.priority}
-          </Typography>
-          <Typography variant="body2">
-            Isäntäkysymys: {question.parentId}
-          </Typography>
-          <Typography variant="body2">
-            Sisältö: {question.text[language]}
-          </Typography>
-        </Box>
-      </Box>
+      <QuestionItem question={question} language={language} />
 
       {childQuestions &&
         childQuestions.map((children) => (
