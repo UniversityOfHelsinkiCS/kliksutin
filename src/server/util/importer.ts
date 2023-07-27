6ee77f9e9d5b38704d3301af6738e296add1ebfe
Henri Remonen <henri@remonen.fi>
2023-07-27 16:37:18 +0300

    Type the importer util functions

--- src/server/util/importer.ts
+++ src/server/util/importer.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { IMPORTER_URL, API_TOKEN } from './config'
+import { Course } from '../types'
 
 export const importerClient = axios.create({
   baseURL: IMPORTER_URL,
@@ -8,14 +9,18 @@ export const importerClient = axios.create({
   },
 })
 
-export const getTeacherCourses = async (userId: string): Promise<any[]> => {
-  const { data } = await importerClient.get(`/courses/${userId}`)
+export const getTeacherCourses = async (userId: string): Promise<Course[]> => {
+  const { data }: { data: Course[] } = await importerClient.get(
+    `/courses/${userId}`
+  )
 
   return data
 }
 
-export const getCourse = async (courseId: string): Promise<any[]> => {
-  const { data } = await importerClient.get(`/course/${courseId}`)
+export const getCourse = async (courseId: string): Promise<Course> => {
+  const { data }: { data: Course } = await importerClient.get(
+    `/course/${courseId}`
+  )
 
   return data
 }
