419e1696061715ca6221bca216f47680d58ef277
Henri Remonen <henri@remonen.fi>
2023-07-28 11:20:18 +0300

    Update the sendReminderEmails function

--- src/server/mailer/mails/sendReminderEmails.ts
+++ src/server/mailer/mails/sendReminderEmails.ts
@@ -16,13 +16,13 @@ const startDateInXMonths = <T extends number>(
 ) => {
   const now = new Date()
 
-  const xMonthsAgo = new Date(startDate)
-  xMonthsAgo.setMonth(xMonthsAgo.getMonth() - XMonths)
+  const inXMonths = new Date(startDate)
+  inXMonths.setMonth(inXMonths.getMonth() - XMonths)
 
-  return now >= xMonthsAgo && now <= startDate
+  return now >= inXMonths && now <= startDate
 }
 
-const sendSummaryReminderEmails = async (surveyId: number) => {
+const sendReminderEmails = async (surveyId: number) => {
   // Find all the entries where reminder email is not sent already
   const newEntries = await Entry.findAll({
     where: {
@@ -33,27 +33,35 @@ const sendSummaryReminderEmails = async (surveyId: number) => {
 
   // Find and combine all the courses that starts in one month with the
   // entries
-  const newSummaries = newEntries.filter(async (entry) => {
-    const courseId = entry.data.course
+  const newSummaries = await Promise.all(
+    newEntries.map(async (entry) => {
+      const courseId = entry.data.course
 
-    if (!courseId) {
-      logger.warn(`Course ID not found in the entry ID: ${entry.id}`)
-      return false
-    }
+      if (!courseId) {
+        logger.warn(`Course ID not found in the entry ID: ${entry.id}`)
+        return null
+      }
 
-    const course = await getCourse(courseId)
+      const course = await getCourse(courseId)
 
-    const startDate = course.activityPeriod?.startDate
+      const startDate = course.activityPeriod?.startDate
 
-    if (!startDate) {
-      logger.warn(`Course start date not found in the course ID: ${courseId}`)
-      return false
-    }
+      if (!startDate) {
+        logger.warn(`Course start date not found in the course ID: ${courseId}`)
+        return null
+      }
 
-    return startDateInXMonths(startDate, 1)
-  })
+      if (startDateInXMonths(startDate, 1)) {
+        // Clone the entry object and add the course property to it
+        Object.assign(entry, { courseData: course })
+        return entry
+      }
 
-  return newSummaries
+      return null
+    })
+  )
+
+  console.log(newSummaries)
 }
 
-export default sendSummaryReminderEmails
+export default sendReminderEmails
