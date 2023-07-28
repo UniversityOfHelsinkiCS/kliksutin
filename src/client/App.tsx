c8db7e18530283bfa7cbb1862fb0a7637780154a
Henri Remonen <henri@remonen.fi>
2023-07-28 15:54:56 +0300

    Migrate old react router dom to newer syntax

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { ThemeProvider } from '@mui/material/styles'
+import { Outlet } from 'react-router-dom'
 import { Box } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
 import { SnackbarProvider } from 'notistack'
 
 import { useTheme } from './theme'
 import Footer from './components/Footer'
 import NavBar from './components/NavBar/NavBar'
-import Router from './Router'
 import { ResultDataProvider } from './contexts/ResultDataContext'
 
 const App = () => {
@@ -15,27 +15,27 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <SnackbarProvider preventDuplicate>
-        <Box
-          sx={{
-            minHeight: '100vh',
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-        >
-          <NavBar />
+        <ResultDataProvider>
           <Box
-            flexGrow={1}
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            minHeight="100vh"
+            sx={{
+              minHeight: '100vh',
+              display: 'flex',
+              flexDirection: 'column',
+            }}
           >
-            <ResultDataProvider>
-              <Router />
-            </ResultDataProvider>
+            <NavBar />
+            <Box
+              flexGrow={1}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              minHeight="100vh"
+            >
+              <Outlet />
+            </Box>
+            <Footer />
           </Box>
-          <Footer />
-        </Box>
+        </ResultDataProvider>
       </SnackbarProvider>
     </ThemeProvider>
   )
--- src/client/Router.tsx
+++ src/client/Router.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom'
 
 import Admin from './components/Admin/Admin'
 import Contact from './components/ContactPage/Contact'
@@ -11,22 +16,30 @@ import EditRecommendations from './components/Admin/EditRecommendations/EditReco
 import EditSurvey from './components/Admin/EditSurvey/EditSurvey'
 import Viewer from './components/Viewer/Viewer'
 
-const Router = () => (
-  <Routes>
-    <Route path="/*" element={<InteractiveForm />} />
-    <Route path="/public" element={<InteractiveForm />} />
-    <Route path="/admin" element={<Admin />}>
-      <Route index element={<EditSurvey />} />
-      <Route path="edit-dimensions" element={<EditDimensions />} />
-      <Route path="edit-questions" element={<EditQuestions />} />
-      <Route path="edit-results" element={<EditResults />} />
-      <Route path="edit-recommendations" element={<EditRecommendations />} />
+import { PUBLIC_URL } from '../config'
+import App from './App'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/" element={<InteractiveForm />} />
+      <Route path="/public" element={<InteractiveForm />} />
+      <Route path="/admin" element={<Admin />}>
+        <Route index element={<EditSurvey />} />
+        <Route path="edit-dimensions" element={<EditDimensions />} />
+        <Route path="edit-questions" element={<EditQuestions />} />
+        <Route path="edit-results" element={<EditResults />} />
+        <Route path="edit-recommendations" element={<EditRecommendations />} />
+      </Route>
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/view">
+        <Route path=":entryId" element={<Viewer />} />
+      </Route>
     </Route>
-    <Route path="/contact" element={<Contact />} />
-    <Route path="/view">
-      <Route path=":entryId" element={<Viewer />} />
-    </Route>
-  </Routes>
+  ),
+  { basename: PUBLIC_URL }
 )
+
+const Router = () => <RouterProvider router={router} />
 
 export default Router
