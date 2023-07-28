f0297312a22714d69eb03aa809d44764094a8c47
Henri Remonen <henri@remonen.fi>
2023-07-28 12:46:56 +0300

    Include user information on the entry query

--- src/server/mailer/mails/sendReminderEmails.ts
+++ src/server/mailer/mails/sendReminderEmails.ts
@@ -1,7 +1,10 @@
-import { Entry } from '../../db/models'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Entry, User } from '../../db/models'
 
 import logger from '../../util/logger'
 import { getCourse } from '../../util/importer'
+
+import { Course } from '../../types'
 
 type PositiveInteger<T extends number> = `${T}` extends
   | '0'
@@ -25,18 +28,14 @@ const startDateInXMonths = <T extends number>(
   return now >= emailTreshold && now <= startDate
 }
 
-const sendReminderEmails = async (surveyId: number) => {
-  // Find all the entries where reminder email is not sent already
-  const newEntries = await Entry.findAll({
-    where: {
-      surveyId,
-      reminderSent: false,
-    },
-  })
+interface UpcomingReminderData extends Entry {
+  courseData: Course
+}
 
-  // Find and combine the courses and entries if the course starts in x months
-  const newSummaries = await Promise.all(
-    newEntries.map(async (entry) => {
+// Find and combine the courses and entries if the course starts in x months
+const upcomingReminderData = (entries: Entry[]) =>
+  Promise.all(
+    entries.map(async (entry) => {
       const courseId = entry.data.course
 
       if (!courseId) {
@@ -56,14 +55,36 @@ const sendReminderEmails = async (surveyId: number) => {
       if (startDateInXMonths(startDate, 1)) {
         // Clone the entry object and add the course property to it
         Object.assign(entry, { courseData: course })
-        return entry
+        return entry as UpcomingReminderData
       }
 
       return null
     })
   )
 
-  console.log(newSummaries)
+const sendReminderEmails = async (surveyId: number) => {
+  // Find all the entries where reminder email is not sent already
+  const newEntries = await Entry.findAll({
+    include: User,
+    where: {
+      surveyId,
+      reminderSent: false,
+    },
+  })
+
+  const entries = await upcomingReminderData(newEntries)
+
+  const entriesToRemind = entries.filter((entry) => entry !== null)
+
+  const emails = entriesToRemind.map((entry) => {
+    const email = {
+      to: '',
+      subject: `Curre reminder for ${entry?.courseData.name.en}`,
+      text: 'This is a reminder email',
+    }
+
+    return email
+  })
 }
 
 export default sendReminderEmails
