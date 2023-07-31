8bb68d8b83c4e0f128618fc4d9ca5d861b5fa2b0
Henri Remonen <henri@remonen.fi>
2023-07-31 09:41:01 +0300

    Reorganize routes

--- src/client/Router.tsx
+++ src/client/Router.tsx
@@ -6,19 +6,19 @@ import {
   createRoutesFromElements,
 } from 'react-router-dom'
 
+import App from './App'
+import ErrorPage from './components/Common/ErrorPage'
+import InteractiveForm from './components/InteractiveForm/InteractiveForm'
 import Admin from './components/Admin/Admin'
 import Contact from './components/ContactPage/Contact'
-import InteractiveForm from './components/InteractiveForm/InteractiveForm'
+import Viewer from './components/Viewer/Viewer'
 import EditDimensions from './components/Admin/EditDimensions/EditDimensions'
 import EditQuestions from './components/Admin/EditQuestions/EditQuestions'
 import EditResults from './components/Admin/EditResults/EditResults'
 import EditRecommendations from './components/Admin/EditRecommendations/EditRecommendations'
 import EditSurvey from './components/Admin/EditSurvey/EditSurvey'
-import Viewer from './components/Viewer/Viewer'
 
 import { PUBLIC_URL } from '../config'
-import App from './App'
-import ErrorPage from './components/Common/ErrorPage'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -33,16 +33,16 @@ const router = createBrowserRouter(
         element={<InteractiveForm />}
         errorElement={<ErrorPage />}
       />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/view">
+        <Route path=":entryId" element={<Viewer />} />
+      </Route>
       <Route path="/admin" element={<Admin />}>
         <Route index element={<EditSurvey />} />
         <Route path="edit-dimensions" element={<EditDimensions />} />
         <Route path="edit-questions" element={<EditQuestions />} />
         <Route path="edit-results" element={<EditResults />} />
         <Route path="edit-recommendations" element={<EditRecommendations />} />
-      </Route>
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/view">
-        <Route path=":entryId" element={<Viewer />} />
       </Route>
     </Route>
   ),
