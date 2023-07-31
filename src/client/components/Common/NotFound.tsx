09df1ee6efe5d5a09eff4b39ef2f665db8e02cef
Henri Remonen <henri@remonen.fi>
2023-07-31 08:43:06 +0300

    Restore eslint rule

--- src/client/components/Common/NotFound.tsx
+++ src/client/components/Common/NotFound.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Box, Button, Container, Grid, Typography } from '@mui/material'
