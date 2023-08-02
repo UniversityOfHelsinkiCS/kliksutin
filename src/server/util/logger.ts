20082993d949ebe566061c99310b247d4829e6aa
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-08-02 13:42:08 +0300

    Fix logger crashing after log server url change

--- src/server/util/logger.ts
+++ src/server/util/logger.ts
@@ -46,7 +46,7 @@ if (!inProduction) {
   transports.push(
     new WinstonGelfTransporter({
       handleExceptions: true,
-      host: 'toska-tmp.cs.helsinki.fi',
+      host: 'toska.cs.helsinki.fi',
       port: 9503,
       protocol: 'udp',
       hostName: os.hostname(),
