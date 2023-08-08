b22c069982f2d90e16a84a3bf78cea141f0d123a
Henri Remonen <henri@remonen.fi>
2023-08-08 08:14:11 +0300

    Update the EditSurvey components and remove old dependencies

--- src/client/components/Admin/EditSurvey/EditSurvey.tsx
+++ src/client/components/Admin/EditSurvey/EditSurvey.tsx
@@ -3,39 +3,17 @@ import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Box } from '@mui/material'
 
-import { Locales, Question } from '@backend/types'
+import { Locales } from '@backend/types'
 
 import useSurvey from '../../../hooks/useSurvey'
 import useQuestions from '../../../hooks/useQuestions'
 
 import RenderQuestions from './RenderQuestions'
-import EditModeControls from './EditModeControls'
 
 const EditSurvey = () => {
   const { i18n } = useTranslation()
   const { survey, isLoading: surveyIsLoading } = useSurvey()
   const { questions, isLoading: questionsIsLoading } = useQuestions(survey?.id)
-  const [inEditMode, setInEditMode] = useState(false)
-  const [selectedQuestion, setSelectedQuestion] = useState<
-    Question | undefined
-  >()
-
-  const onCancel = () => {
-    setInEditMode(false)
-    setSelectedQuestion(undefined)
-  }
-
-  useEffect(() => {
-    const close = (e: KeyboardEvent) => {
-      if (e.key === 'Escape') {
-        onCancel()
-      }
-    }
-
-    window.addEventListener('keydown', close)
-
-    return () => window.removeEventListener('keydown', close)
-  }, [])
 
   if (!survey || surveyIsLoading || !questions || questionsIsLoading)
     return null
@@ -53,20 +31,10 @@ const EditSurvey = () => {
               question={question}
               questions={sortedQuestions}
               language={language as keyof Locales}
-              inEditMode={inEditMode}
-              setInEditMode={setInEditMode}
-              selectedQuestion={selectedQuestion}
-              setSelectedQuestion={setSelectedQuestion}
             />
           )}
         </div>
       ))}
-
-      <EditModeControls
-        questionName={selectedQuestion?.title[language as keyof Locales]}
-        isOpen={inEditMode}
-        onClose={onCancel}
-      />
     </Box>
   )
 }
--- src/client/components/Admin/EditSurvey/QuestionItem.tsx
+++ src/client/components/Admin/EditSurvey/QuestionItem.tsx
@@ -1,102 +1,159 @@
 import React from 'react'
-import { Badge, Box, Chip, InputLabel, Tooltip } from '@mui/material'
+import { enqueueSnackbar } from 'notistack'
+import { useTranslation } from 'react-i18next'
+import {
+  Badge,
+  Box,
+  Chip,
+  IconButton,
+  InputLabel,
+  Tooltip,
+} from '@mui/material'
 
 import FingerprintIcon from '@mui/icons-material/Fingerprint'
 import LowPriorityIcon from '@mui/icons-material/LowPriority'
 import ChildCareIcon from '@mui/icons-material/ChildCare'
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 
 import { Locales, Question } from '@backend/types'
+
+import { useEditQuestionPriorityMutation } from '../../../hooks/useQuestionMutation'
+
 import QuestionItemMenu from './QuestionItemMenu'
+
+import { UpdatedQuestionLocation } from '../../../../validators/questions'
+
+interface MoveHandleProps {
+  question: Question
+  questions: Question[]
+}
 
 interface QuestionsProps {
   question: Question
+  questions: Question[]
   language: keyof Locales
-  inEditMode: boolean
-  handleStartPositionChange: () => void
 }
 
-const QuestionItem = ({
-  question,
-  language,
-  inEditMode,
-  handleStartPositionChange,
-}: QuestionsProps) => {
-  const borderColor = inEditMode ? 'grey.300' : 'grey.400'
-  const textColor = inEditMode ? 'grey.400' : 'black'
+const MoveHandles = ({ question, questions }: MoveHandleProps) => {
+  const { t } = useTranslation()
+  const mutation = useEditQuestionPriorityMutation(question.id)
+
+  const parentChildQuestions = questions.filter(
+    (q) => q.parentId === question.parentId
+  )
+
+  const handleChangePosition = async (
+    destinationData: UpdatedQuestionLocation
+  ) => {
+    try {
+      await mutation.mutateAsync(destinationData)
+
+      enqueueSnackbar(t('admin:rePrioritizeSuccess'), { variant: 'success' })
+    } catch (error: any) {
+      enqueueSnackbar(error.message, { variant: 'error' })
+    }
+  }
+
+  if (parentChildQuestions.length <= 1) return null
 
   return (
-    <Box
-      key={question.id}
-      sx={{
-        p: 2,
-        my: 4,
-        border: 1,
-        borderColor,
-        color: textColor,
-        position: 'relative',
-        ...(inEditMode
-          ? {} // Empty object when inEditMode is true, so no additional styles are applied
-          : {
-              '&:hover': {
-                border: 1,
-                borderColor: '#0288d1',
-              },
-            }),
-      }}
-    >
-      <InputLabel
-        sx={{
-          mt: '-1.75em',
-          px: '0.5em',
-          zIndex: 2,
-          width: 'full',
-          backgroundColor: 'white',
-          position: 'absolute',
-          color: textColor,
-        }}
-      >
-        {question.title[language]}
-      </InputLabel>
-      <Box sx={{ mt: 1 }}>
-        <QuestionItemMenu
-          question={question}
-          handleStartPositionChange={handleStartPositionChange}
-        />
-        <Tooltip title="The number represents the unique ID of the question">
-          <Badge sx={{ mr: 1 }} badgeContent={question.id} color="primary">
-            <FingerprintIcon />
-          </Badge>
-        </Tooltip>
-        {question.parentId && (
-          <Tooltip title="This badge represents that the question is a child question. The number represents the ID number of the parent question">
-            <Badge
-              sx={{ mr: 1 }}
-              badgeContent={question.parentId}
-              color="primary"
-            >
-              <ChildCareIcon />
-            </Badge>
-          </Tooltip>
-        )}
-        <Tooltip title="Priority determines the order in which the questions appear on the survey. 0 being the first question. Child question priorities starts from 0 but their priorities are only valid inside the child tree">
-          <Chip
-            sx={{
-              mx: 2,
-              px: '0.3rem',
-              fontWeight: 'normal',
-              borderRadius: '1rem',
-              color: textColor,
-            }}
-            label={`Priority: ${question.priority}`}
-            color="primary"
-            variant="outlined"
-            size="small"
-            icon={<LowPriorityIcon sx={{ color: textColor }} />}
-          />
-        </Tooltip>
-      </Box>
+    <Box sx={{ mr: 2 }}>
+      {question.priority !== 0 && (
+        <IconButton
+          size="small"
+          onClick={() =>
+            handleChangePosition({
+              parentId: question.parentId,
+              priority: question.priority - 1,
+            })
+          }
+        >
+          <KeyboardArrowUpIcon />
+        </IconButton>
+      )}
+      {question.priority < parentChildQuestions.length - 1 && (
+        <IconButton
+          size="small"
+          onClick={() =>
+            handleChangePosition({
+              parentId: question.parentId,
+              priority: question.priority + 1,
+            })
+          }
+        >
+          <KeyboardArrowDownIcon />
+        </IconButton>
+      )}
     </Box>
   )
 }
+
+const QuestionItem = ({ question, questions, language }: QuestionsProps) => (
+  <Box
+    key={question.id}
+    sx={{
+      p: 2,
+      my: 4,
+      border: 1,
+      position: 'relative',
+      '&:hover': {
+        border: 1,
+        borderColor: '#0288d1',
+      },
+    }}
+  >
+    <InputLabel
+      sx={{
+        mt: '-1.75em',
+        px: '0.5em',
+        zIndex: 2,
+        width: 'full',
+        backgroundColor: 'white',
+        position: 'absolute',
+      }}
+    >
+      {question.title[language]}
+    </InputLabel>
+    <Box sx={{ mt: 1, display: 'flex' }}>
+      <MoveHandles question={question} questions={questions} />
+      {/* <QuestionItemMenu
+          question={question}
+          handleStartPositionChange={handleStartPositionChange}
+        /> */}
+      <Tooltip title="The number represents the unique ID of the question">
+        <Badge sx={{ mr: 1 }} badgeContent={question.id} color="primary">
+          <FingerprintIcon />
+        </Badge>
+      </Tooltip>
+      {question.parentId && (
+        <Tooltip title="This badge represents that the question is a child question. The number represents the ID number of the parent question">
+          <Badge
+            sx={{ mr: 1 }}
+            badgeContent={question.parentId}
+            color="primary"
+          >
+            <ChildCareIcon />
+          </Badge>
+        </Tooltip>
+      )}
+      <Tooltip title="Priority determines the order in which the questions appear on the survey. 0 being the first question. Child question priorities starts from 0 but their priorities are only valid inside the child tree">
+        <Chip
+          sx={{
+            mx: 2,
+            px: '0.3rem',
+            fontWeight: 'normal',
+            borderRadius: '1rem',
+          }}
+          label={`Priority: ${question.priority}`}
+          color="primary"
+          variant="outlined"
+          size="small"
+          icon={<LowPriorityIcon />}
+        />
+      </Tooltip>
+    </Box>
+  </Box>
+)
 
 export default QuestionItem
