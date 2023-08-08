2b1f9e3982fb44a37bbb1d0780e354968c449341
Henri Remonen <henri@remonen.fi>
2023-08-08 11:54:44 +0300

    Add route for editing survey information

--- src/server/routes/survey.ts
+++ src/server/routes/survey.ts
@@ -1,6 +1,10 @@
 import express from 'express'
 
+import { RequestWithUser } from '@backend/types'
+
 import { Survey, Question } from '../db/models'
+
+import { UpdatedSurveyInfoZod } from '../../validators/survey'
 
 const surveyRouter = express.Router()
 
@@ -21,6 +25,35 @@ surveyRouter.get('/:name', async (req, res) => {
   if (!survey) throw new Error('Survey not found')
 
   survey.Questions = survey.Questions.sort(sortByPriority)
+
+  return res.send(survey)
+})
+
+surveyRouter.put('/:name', async (req: RequestWithUser, res: any) => {
+  const { name } = req.params
+  const { isAdmin } = req.user
+
+  if (!isAdmin) throw new Error('Unauthorized')
+
+  const survey = await Survey.findOne({
+    where: {
+      name,
+    },
+    include: {
+      model: Question,
+    },
+  })
+
+  if (!survey) throw new Error('Survey not found')
+
+  const request = UpdatedSurveyInfoZod.safeParse(req.body)
+
+  if (!request.success) throw new Error('Validation failed')
+  const body = request.data
+
+  Object.assign(survey, body)
+
+  await survey.save()
 
   return res.send(survey)
 })
