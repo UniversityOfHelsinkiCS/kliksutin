cf4435f28fb1ef797a1bc80086d6a8100e292619
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-08-10 08:43:59 +0300

    Only use test services in staging

--- src/server/util/config.ts
+++ src/server/util/config.ts
@@ -14,14 +14,14 @@ if (inProduction || inStaging) connectionString = `${connectionString}&ssl=true`
 
 export const DB_CONNECTION_STRING = connectionString
 
-export const JAMI_URL = inProduction
-  ? 'https://api-toska.apps.ocp-prod-0.k8s.it.helsinki.fi/jami/'
-  : 'https://api-toska.apps.ocp-test-0.k8s.it.helsinki.fi/jami'
+export const JAMI_URL = inStaging
+  ? 'https://api-toska.apps.ocp-test-0.k8s.it.helsinki.fi/jami/'
+  : 'https://api-toska.apps.ocp-prod-0.k8s.it.helsinki.fi/jami'
 
-export const PATE_URL = inProduction
-  ? 'https://api-toska.apps.ocp-prod-0.k8s.it.helsinki.fi/pate/'
-  : 'https://api-toska.apps.ocp-test-0.k8s.it.helsinki.fi/pate/'
+export const PATE_URL = inStaging
+  ? 'https://api-toska.apps.ocp-test-0.k8s.it.helsinki.fi/pate/'
+  : 'https://api-toska.apps.ocp-prod-0.k8s.it.helsinki.fi/pate/'
 
-export const IMPORTER_URL = inProduction
-  ? 'https://api-toska.apps.ocp-prod-0.k8s.it.helsinki.fi/importer/kliksutin'
-  : 'https://api-toska.apps.ocp-test-0.k8s.it.helsinki.fi/importer/kliksutin'
+export const IMPORTER_URL = inStaging
+  ? 'https://api-toska.apps.ocp-test-0.k8s.it.helsinki.fi/importer/kliksutin'
+  : 'https://api-toska.apps.ocp-prod-0.k8s.it.helsinki.fi/importer/kliksutin'
