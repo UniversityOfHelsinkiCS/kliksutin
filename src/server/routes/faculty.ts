0ff224196efa7ca03580e5c97b1e92f52676adff
Henri Remonen <henri@remonen.fi>
2023-08-21 09:04:36 +0300

    Extract faculty route business logic into service

--- src/server/routes/faculty.ts
+++ src/server/routes/faculty.ts
@@ -1,28 +1,12 @@
 import express from 'express'
 
-import { inE2EMode } from '../../config'
+import { getFaculties, getUserFaculties } from '../services/faculty'
 import { RequestWithUser } from '../types'
-import { getOrganisationData, getUserOrganisations } from '../util/jami'
-
-const mockFaculty = [
-  {
-    code: 'H50',
-    name: {
-      fi: 'Matemaattis-luonnontieteellinen tiedekunta',
-      en: 'Faculty of Science',
-      sv: 'Matematisk-naturvetenskapliga fakulteten',
-    },
-  },
-]
 
 const facultyRouter = express.Router()
 
 facultyRouter.get('/', async (req, res) => {
-  if (inE2EMode) return res.send(mockFaculty)
-
-  const organisationData = (await getOrganisationData()) || []
-
-  const faculties = organisationData.map(({ code, name }) => ({ code, name }))
+  const faculties = await getFaculties()
 
   return res.send(faculties)
 })
@@ -30,13 +14,7 @@ facultyRouter.get('/', async (req, res) => {
 facultyRouter.get('/user', async (req: RequestWithUser, res: any) => {
   const { id, iamGroups = [] } = req.user
 
-  if (inE2EMode) return res.send(mockFaculty)
-
-  if (!id) return res.send([])
-
-  const organisationData = await getUserOrganisations(id, iamGroups)
-
-  const faculties = organisationData.map(({ code, name }) => ({ code, name }))
+  const faculties = await getUserFaculties(id, iamGroups)
 
   return res.send(faculties)
 })
