c9ce479326c842b46a2cf6bf39430d7ca09aaaae
Henri Remonen <henri@remonen.fi>
2023-09-14 09:13:43 +0300

    feat(admin): Create initial functionality for admins to be able to change recommendation's related dimensions

--- src/client/components/Admin/EditRecommendations/EditRecommendations.tsx
+++ src/client/components/Admin/EditRecommendations/EditRecommendations.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import {
   useLocation,
@@ -6,14 +6,15 @@ import {
   useParams,
   useSearchParams,
 } from 'react-router-dom'
-import { Box, Typography } from '@mui/material'
+import { Box, Button, Typography } from '@mui/material'
 
-import { Locales } from '@backend/types'
+import { DimensionSelectionData, Locales } from '@backend/types'
 
 import useSurvey from '../../../hooks/useSurvey'
 import useRecommendations from '../../../hooks/useRecommendations'
 
 import EditRecommendation from './EditRecommendation'
+import { MultiDimensionSelect } from '../Select'
 
 const EditRecommendations = () => {
   const { t } = useTranslation()
@@ -22,15 +23,42 @@ const EditRecommendations = () => {
   const { recommendationId } = useParams()
   const [searchParams] = useSearchParams()
   const { survey } = useSurvey()
-  const { recommendations, isSuccess } = useRecommendations(survey?.id)
-
-  if (!isSuccess || !recommendations) return null
+  const { recommendations } = useRecommendations(survey?.id)
+  const [dimensionSelection, setDimensionSelection] = useState({})
 
   const selectedLanguage = searchParams.get('transLang') as keyof Locales
 
-  const selectedRecommendation = recommendations.find(
+  const selectedRecommendation = recommendations?.find(
     ({ id }) => id === Number(recommendationId)
   )
+  const dimensionQuestion = survey?.Questions.find(
+    (question) => question.optionData.type === 'dimensions'
+  )
+
+  useEffect(() => {
+    if (!dimensionQuestion) return
+
+    const dimensionQuestionOptions = dimensionQuestion?.optionData
+      .options as unknown as DimensionSelectionData[]
+
+    const oldDimensions = dimensionQuestionOptions.filter((dimension) =>
+      dimension?.data?.some(
+        (dataItem) =>
+          dataItem.recommendationLabel === selectedRecommendation?.label
+      )
+    )
+
+    const selectedDimensions: Record<string, boolean> = oldDimensions.reduce(
+      (result, dimension) => {
+        // eslint-disable-next-line no-param-reassign
+        result[dimension.id] = true
+        return result
+      },
+      {} as Record<string, boolean>
+    )
+
+    setDimensionSelection(selectedDimensions)
+  }, [survey, selectedRecommendation, dimensionQuestion])
 
   const handleRecommendationDeletion = () => {
     navigate({
@@ -40,12 +68,23 @@ const EditRecommendations = () => {
   }
 
   return (
-    <Box width="100%" flexWrap="wrap">
+    <Box width='100%' flexWrap='wrap'>
       {recommendationId && selectedRecommendation ? (
         <Box sx={{ my: 4 }}>
-          <Typography sx={{ my: 4, pl: 1 }} variant="h4">
+          <Typography sx={{ my: 4, pl: 1 }} variant='h4'>
             {t('admin:recommendationViewRecommendationEdit')}
           </Typography>
+          <Box sx={{ mx: 4 }}>
+            <MultiDimensionSelect
+              label={t('admin:selectRecommendationDimensions')}
+              dimensionQuestion={dimensionQuestion}
+              value={dimensionSelection}
+              setValue={setDimensionSelection}
+            />
+            <Button variant='outlined' onClick={() => {}}>
+              {t('admin:save')}
+            </Button>
+          </Box>
           <EditRecommendation
             language={selectedLanguage}
             recommendation={selectedRecommendation}
@@ -53,7 +92,7 @@ const EditRecommendations = () => {
           />
         </Box>
       ) : (
-        <Typography sx={{ my: 4, pl: 1 }} variant="h4">
+        <Typography sx={{ my: 4, pl: 1 }} variant='h4'>
           {t('admin:recommendationViewInfo')}
         </Typography>
       )}
