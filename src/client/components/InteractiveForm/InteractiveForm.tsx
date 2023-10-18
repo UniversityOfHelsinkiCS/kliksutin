35559a0025e4249eea219090b0ea65fb7b99edd0
Henri Remonen <henri@remonen.fi>
2023-10-18 10:58:16 +0300

    chore: do not send ai request email if user missing or if the user did not want to use AI in their course

--- src/client/components/InteractiveForm/InteractiveForm.tsx
+++ src/client/components/InteractiveForm/InteractiveForm.tsx
@@ -2,32 +2,43 @@ import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
 import { enqueueSnackbar } from 'notistack'
+import ReactDOMServer from 'react-dom/server'
 import { Box, Grid } from '@mui/material'
+import { Locales } from '@backend/types'
 
 import useSurvey from '../../hooks/useSurvey'
+import useFaculties from '../../hooks/useFaculties'
 import usePersistForm from '../../hooks/usePersistForm'
+import useLoggedInUser from '../../hooks/useLoggedInUser'
 import useSaveEntryMutation from '../../hooks/useSaveEntryMutation'
 
 import HelloBanner from './HelloBanner'
 import RenderSurvey from './RenderSurvey'
 import Recommendations from '../Recommendations/Recommendations'
 import Results from '../ResultPage/Results'
+import AIRequestEmailTemplate from '../../templates/AIRequestTemplate'
 
 import { useResultData } from '../../contexts/ResultDataContext'
+
+import sendEmail from '../../util/mailing'
 
 import styles from '../../styles'
 import { FormValues } from '../../types'
 import { FORM_DATA_KEY } from '../../../config'
 
 const InteractiveForm = () => {
-  const { survey, isLoading } = useSurvey()
-  const { t } = useTranslation()
+  const { t, i18n } = useTranslation()
+  const { faculties, isLoading: facultiesIsLoading } = useFaculties()
+  const { survey, isLoading: surveyIsLoading } = useSurvey()
+  const { user, isLoading: userIsLoading } = useLoggedInUser()
   const mutation = useSaveEntryMutation(survey?.id)
 
   const sessionLocation = sessionStorage.getItem('curre-session-location')
   const [showResults, setShowResults] = useState(sessionLocation === 'results')
 
   const { resultData, setResultData } = useResultData()
+
+  const { language } = i18n
 
   const { formStyles } = styles
 
@@ -45,7 +56,31 @@ const InteractiveForm = () => {
   const onSubmit = (submittedData: FormValues) => {
     setResultData(submittedData)
 
-    console.log(submittedData)
+    const { useAI, faculty } = submittedData
+
+    if (useAI?.value && user?.email) {
+      const userFaculty = faculties?.find((f) => f.code === faculty)
+
+      const targets = [user?.email, 'opetusteknologia@helsinki.fi']
+
+      const requestEmailTemplate = ReactDOMServer.renderToString(
+        <AIRequestEmailTemplate
+          user={user}
+          faculty={userFaculty?.name[language as keyof Locales]}
+          content={useAI?.content}
+        />
+      )
+
+      sendEmail(targets, requestEmailTemplate, 'Curre Chat Request')
+        .then(() => {
+          enqueueSnackbar(t('contact:sendSuccess'), {
+            variant: 'success',
+          })
+        })
+        .catch(() => {
+          enqueueSnackbar(t('contact:pateErrorMessage'), { variant: 'error' })
+        })
+    }
 
     mutation
       .mutateAsync(submittedData)
@@ -66,7 +101,7 @@ const InteractiveForm = () => {
 
   usePersistForm({ value: watch(), sessionStorageKey: FORM_DATA_KEY })
 
-  if (isLoading) return null
+  if (facultiesIsLoading || surveyIsLoading || userIsLoading) return null
 
   return (
     <Box sx={formStyles.formWrapper}>
