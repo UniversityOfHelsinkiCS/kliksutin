5ffb0b48e471df9d036c180f7e0c089b654efa93
Henri Remonen <henri@remonen.fi>
2024-07-12 11:18:29 +0300

    chore: update cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,7 @@ export default defineConfig({
   viewportWidth: 1280,
   viewportHeight: 720,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:8000',
+    experimentalRunAllSpecs: true,
   },
 })
