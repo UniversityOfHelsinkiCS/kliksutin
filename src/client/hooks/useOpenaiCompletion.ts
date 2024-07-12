2104f674a0ae2dd271904c4edd4f40075194a137
Henri Remonen <henri@remonen.fi>
2024-07-12 11:19:25 +0300

    chore: removed unused import

--- src/client/hooks/useOpenaiCompletion.ts
+++ src/client/hooks/useOpenaiCompletion.ts
@@ -1,5 +1,4 @@
 import { useQuery } from '@tanstack/react-query'
-import { enqueueSnackbar } from 'notistack'
 import { useTranslation } from 'react-i18next'
 
 import apiClient from '../util/apiClient'
