5d949efceba8504b08a88c8f19d1a2027d069092
Henri Remonen <henri@remonen.fi>
2024-07-15 13:58:58 +0300

    feat: sentry in staging

--- src/client/util/sentry.ts
+++ src/client/util/sentry.ts
@@ -2,14 +2,14 @@ import * as Sentry from '@sentry/browser'
 import { inProduction, inStaging, GIT_SHA } from '../../config'
 
 const initializeSentry = () => {
-  if (!inProduction || !inStaging) return
-
-  Sentry.init({
-    dsn: 'https://72e49107b76381db56da800209363389@toska.cs.helsinki.fi/12',
-    release: GIT_SHA,
-    integrations: [Sentry.browserTracingIntegration()],
-    tracesSampleRate: 1.0,
-  })
+  if (inProduction || inStaging) {
+    Sentry.init({
+      dsn: 'https://72e49107b76381db56da800209363389@toska.cs.helsinki.fi/12',
+      release: GIT_SHA,
+      integrations: [Sentry.browserTracingIntegration()],
+      tracesSampleRate: 1.0,
+    })
+  }
 }
 
 export default initializeSentry
--- src/server/util/sentry.ts
+++ src/server/util/sentry.ts
@@ -3,17 +3,17 @@ import * as Sentry from '@sentry/node'
 import { inProduction, inStaging, GIT_SHA } from '../../config'
 
 const initializeSentry = () => {
-  if (!inProduction || !inStaging) return
-
-  Sentry.init({
-    dsn: 'https://72e49107b76381db56da800209363389@toska.cs.helsinki.fi/12',
-    release: GIT_SHA,
-    integrations: [
-      Sentry.httpIntegration({ breadcrumbs: true }),
-      Sentry.expressIntegration(),
-    ],
-    tracesSampleRate: 1.0,
-  })
+  if (inProduction || inStaging) {
+    Sentry.init({
+      dsn: 'https://72e49107b76381db56da800209363389@toska.cs.helsinki.fi/12',
+      release: GIT_SHA,
+      integrations: [
+        Sentry.httpIntegration({ breadcrumbs: true }),
+        Sentry.expressIntegration(),
+      ],
+      tracesSampleRate: 1.0,
+    })
+  }
 }
 
 export default initializeSentry
