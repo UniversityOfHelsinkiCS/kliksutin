84423abc665241f2486bf489301d59fa0103ce93
matiassinisalo <matias.aleksanteri@hotmail.fi>
2025-10-10 14:37:34 +0300

    update client to use /kuraattori when talking to backend

--- src/client/util/apiClient.ts
+++ src/client/util/apiClient.ts
@@ -5,8 +5,8 @@ import { PUBLIC_URL } from '../../config'
 const isPublicVersion = window.location.href.includes('/public')
 
 const baseURL = isPublicVersion
-  ? `${PUBLIC_URL}/public/api`
-  : `${PUBLIC_URL}/api`
+  ? `${PUBLIC_URL}/kuraattori/public/api`
+  : `${PUBLIC_URL}/kuraattori/api`
 
 const apiClient = axios.create({ baseURL })
 
